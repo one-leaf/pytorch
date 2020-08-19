8b7b22446fe6f4153151fd1d79fa58790071730f
oneleaf <wp@bsw.com.cn>
2020-08-19 17:47:43 +0800

    .

--- 04/14.py
+++ 04/14.py
@@ -341,7 +341,9 @@ for i_episode in range(num_episodes):
     # 更新目标网络，复制DQN中的所有权重和偏差
     if i_episode % TARGET_UPDATE == 0 and loss!=None:
         target_net.load_state_dict(policy_net.state_dict())
-        print(i_episode, steps_done, t, '/' , avg_step, "loss:", loss.item(), "reward_proportion", reward_proportion, "position:",memory.position)
+        print(i_episode, steps_done, t, '/' , avg_step, "loss:", loss.item(), "reward_proportion", \
+            reward_proportion, "position:",memory.position,"eps_threshold:",EPS_END + (EPS_START - EPS_END) * \
+            math.exp(-1. * steps_done / EPS_DECAY)
         torch.save({    'policy_net': policy_net.state_dict(),
                     'steps_done': steps_done,
                     'avg_step': avg_step,
