7885af1fc35c7aee15bbd0287758a4ef74426a31
oneleaf <wp@bsw.com.cn>
2020-08-19 17:06:46 +0800

    .

--- 04/14.py
+++ 04/14.py
@@ -316,10 +316,10 @@ for i_episode in range(num_episodes):
             next_state = None
 
         # 在记忆中存储过渡,但减少为1的奖励
-        if random.random()<reward_proportion*2 and not done :
-            if len(memory.memory)==memory.capacity: 
-                while memory.memory[memory.position].next_state==None:
-                    memory.position = (memory.position + 1) % memory.capacity
+        # if random.random()<reward_proportion*2 and not done :
+        #     if len(memory.memory)==memory.capacity: 
+        #         while memory.memory[memory.position].next_state==None:
+        #             memory.position = (memory.position + 1) % memory.capacity
 
         memory.push(state, action, next_state, reward)
 
