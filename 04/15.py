a6192726c1be28d9a3d8e069e318b59ca8ee732d
oneleaf <oneleaf@gmail.com>
2020-08-22 21:18:39 +0800

    .

--- 04/15.py
+++ 04/15.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         'epsi_high': 0.9,
         'epsi_low': 0.05,
         'decay': 200,
-        'lr': 1e-3,
+        'lr': 1e-6,
         'capacity': 100000,
         'batch_size': 256,
         'state_space_dim': env.observation_space.shape[0],
