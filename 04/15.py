99770076980b39580e842327cebee6f93178048c
oneleaf <oneleaf@gmail.com>
2020-08-22 19:25:54 +0800

    .

--- 04/15.py
+++ 04/15.py
@@ -109,16 +109,18 @@ if __name__ == '__main__':
     mean = []
 
     avg_reward = 0.1
+    _reward = -1.        
     for episode in range(100000):
         s0 = env.reset()
-        loss = 0        
         for t in count():
             # env.render()
             a0 = agent.act(s0)
             s1, r1, done, _ = env.step(a0)
             
             if done:
-                r1 = -1. * loss 
+                if loss!=None and loss.item()>1:
+                    _reward *= 1.01
+                r1 = _reward 
             else:
                 r1 = math.exp(-1. * (t+1) / avg_reward )
 
