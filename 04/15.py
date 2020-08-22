80b3588169f7ab74d4fd30ff14bac031d4d8f477
oneleaf <oneleaf@gmail.com>
2020-08-22 19:15:06 +0800

    .

--- 04/15.py
+++ 04/15.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     env = gym.make('CartPole-v0').unwrapped
 
     params = {
-        'gamma': 0.5, 
+        'gamma': 0.9, 
         'epsi_high': 0.9,
         'epsi_low': 0.05,
         'decay': 200,
