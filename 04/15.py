c013c08b548ae31ec09ee02bbcbbc85330b05c6a
oneleaf <oneleaf@gmail.com>
2020-08-22 18:38:16 +0800

    。

--- 04/15.py
+++ 04/15.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
             if not done:
                 r1 = -1.
             else:
-                r1 = math.exp(-1. * (t+1) / avg_reward )
+                r1 = t # math.exp(-1. * (t+1) / avg_reward )
 
             agent.put(s0, a0, r1, s1)
             
