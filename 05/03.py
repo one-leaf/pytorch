7bce61b84823957f2f1930cef3a99190c52aaccb
oneleaf <wp@bsw.com.cn>
2020-08-25 18:36:22 +0800

    .

--- 05/03.py
+++ 05/03.py
@@ -118,16 +118,14 @@ class Agent(object):
 class Net(nn.Module):
     def __init__(self, hidden_size, output_size):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(96, hidden_size)
+        self.conv1 = nn.Conv2d(3, 16, 3, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, 2)
+        self.fc1 = nn.Linear(128, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 96)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 128)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
