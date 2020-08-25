68e89f66600e596199ebe81bb70848ef00cec816
oneleaf <wp@bsw.com.cn>
2020-08-25 17:26:09 +0800

    。

--- 05/02.py
+++ 05/02.py
@@ -136,7 +136,7 @@ TARGET_UPDATE = 10
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 n_actions = 4 
-buffer = deque(maxlen=1000000)
+buffer = deque(maxlen=1000)
 modle_file = 'data/save/05_02_checkpoint.tar'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 steps_done = 0
