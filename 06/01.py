f704a7d9d0a3f1f449cff3abd8d829eb2a469bf4
oneleaf <wp@bsw.com.cn>
2020-08-27 12:05:42 +0800

    .

--- 06/01.py
+++ 06/01.py
@@ -1,11 +1,8 @@
 import gym
-import logging
-import numpy
 import random
-from gym import spaces
+from gym.envs.classic_control import rendering
+
 import time
- 
-logger = logging.getLogger(__name__)
  
 class FiveChessEnv(gym.Env):
     metadata = {
@@ -13,14 +10,18 @@ class FiveChessEnv(gym.Env):
         'video.frames_per_second': 2
     }
  
-    def __init__(self, size=15):
-        #棋盘大小
+    def __init__(self, size=15, n_in_row=5):
+        # 棋盘大小
         self.SIZE = size
-        #初始棋盘是0    -1表示黑棋子   1表示白棋子
+        # 最少多少子连线才算赢
+        self.n_in_row = n_in_row
+        # 初始棋盘是0    -1表示黑棋子   1表示白棋子
         self.chessboard = [ [  0 for v in range(self.SIZE)  ] for v in range(self.SIZE) ]
         self.viewer = None
         self.step_count = 0
-
+        self.players = [1, -1]
+        self.reset()
+        
     def is_valid_coord(self,x,y):
         return x>=0 and x<self.SIZE and y>=0 and y<self.SIZE
  
@@ -36,182 +37,54 @@ class FiveChessEnv(gym.Env):
                     results.append([x,y])
         return results
  
-    #action 包括坐标和棋子颜色  例如：[1,3,1] 表示： 坐标（1,3），白棋
-    #输出 下一个状态，动作价值，是否结束，额外信息{}
+    # 获取当前用户
+    def get_current_player(self):
+        return self.step_count%2
+
+    # 检查是否游戏结束,返回赢的用户0 或 1，如果平局返回-1
+    def check_terminal(self):
+        # 如果都没有下子的位置了，则返回平局
+        if len(self.get_available_locations())==0:
+            return True, -1
+
+        # 遍历落子位置，检查是否出现横/竖/斜线上n子相连的情况
+        n = self.n_in_row
+        for x in range(self.SIZE):
+            for y in range(self.SIZE):
+                color = self.chessboard[x][y]
+                if color == 0: continue
+                if self.SIZE-x>=n and abs(sum([self.chessboard[x+i][y] for i in range(n)]))==n:  
+                    return True, self.players.index(color)
+
+                if self.SIZE-y>=n and abs(sum([self.chessboard[x][y+i] for i in range(n)]))==n:  
+                    return True, self.players.index(color)
+
+                if self.SIZE-x>=n and self.SIZE-y>=n and abs(sum([self.chessboard[x+i][y+i] for i in range(n)]))==n:  
+                    return True, self.players.index(color)
+
+                if x>=n and y>=n and abs(sum([self.chessboard[x-i][y-i] for i in range(n)]))==n:  
+                    return True, self.players.index(color)
+        return False, -1        
+
+    #action 包括坐标和  例如：[1,3] 表示： 坐标（1,3）
+    #输出 下一个状态，动作价值，是否结束，赢的用户
     def step(self, action):
-        '''
-        #非法操作
-        if not self.is_valid_set_coord(action[0],action[1]):
-            return self.chessboard,-50,False,{}
-        '''
- 
-        #棋子
-        self.chessboard[action[0]][action[1]] = action[2]
- 
         self.step_count +=1
- 
+
         #胜负判定
-        color = action[2]
+        color = self.players[self.get_current_player()]
+
+        #棋子
+        self.chessboard[action[0]][action[1]] = color
         
-        win_reward = 1000
-        common_reward = -20
-        draw_reward = 0
- 
-        #1.横向
-        count = 1
-        win = False
- 
-        i = 1
-        stop0 = False
-        stop1 = False
- 
-        while i<self.SIZE:
-            x = action[0]+i
-            y = action[1]
-            #左边
-            if (not stop0) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop0 = True
-            #右边
-            x = action[0]-i
-            if (not stop1) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop1 = True
- 
-            #超过5个相同，则胜利
-            if count>=5:
-                win = True
-                break
- 
-            #都不相同，停止探索
-            if stop0 and stop1:
-                break
-            i+=1
- 
-        if win:
-            print('win1')
-            return self.chessboard,win_reward,True,{}
-        #2.纵向
-        count = 1
-        win = False
- 
-        i = 1
-        stop0 = False
-        stop1 = False
- 
-        while i<self.SIZE:
-            x = action[0]
-            y = action[1]+i
-            #左边
-            if (not stop0) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop0 = True
-            #右边
-            y = action[1]-i
-            if (not stop1) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop1 = True
- 
-            #超过5个相同，则胜利
-            if count>=5:
-                win = True
-                break
- 
-            #都不相同，停止探索
-            if stop0 and stop1:
-                break
-            i+=1
-        if win:
-            print('win2')
-            return self.chessboard,win_reward,True,{}
-        #3.左斜向
-        count = 1
-        win = False
- 
-        i = 1
-        stop0 = False
-        stop1 = False
- 
-        while i<self.SIZE:
-            x = action[0]+i
-            y = action[1]+i
-            #左边
-            if (not stop0) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop0 = True
-            #右边
-            x = action[0]-i
-            y = action[1]-i
-            if (not stop1) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop1 = True
- 
-            #超过5个相同，则胜利
-            if count>=5:
-                win = True
-                break
- 
-            #都不相同，停止探索
-            if stop0 and stop1:
-                break
-            i+=1
-        if win:
-            print('win3')
-            return self.chessboard,win_reward,True,{}
- 
-        #3.右斜向
-        count = 1
-        win = False
- 
-        i = 1
-        stop0 = False
-        stop1 = False
- 
-        while i<self.SIZE:
-            x = action[0]-i
-            y = action[1]+i
-            #左边
-            if (not stop0) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop0 = True
-            #右边
-            x = action[0]+i
-            y = action[1]-i
-            if (not stop1) and self.is_valid_coord(x,y) and self.chessboard[x][y] == color:
-                count = count+1
-            else:
-                stop1 = True
- 
-            #超过5个相同，则胜利
-            if count>=5:
-                win = True
-                break
- 
-            #都不相同，停止探索
-            if stop0 and stop1:
-                break
-            i+=1
-            
-        if win:
-            print('win4')
-            return self.chessboard,win_reward,True,{}
- 
-        if self.step_count == self.SIZE*self.SIZE:
-            print('draw')
-            return self.chessboard,draw_reward,True,{}
- 
-        return self.chessboard,common_reward,False,{}
- 
+        terminal, user = self.check_terminal()
+        reward = 0 if user==-1 else 1 
+        return self.chessboard,reward,terminal,user
+
     def reset(self):
         self.chessboard = [ [  0 for v in range(self.SIZE)  ] for v in range(self.SIZE) ]
         self.step_count = 0
+        self.current_player = self.players[0]
         return self.chessboard
  
     def render(self, mode = 'human', close=False):
@@ -227,7 +100,6 @@ class FiveChessEnv(gym.Env):
         width = (screen_width - space*2)/(self.SIZE-1)
  
         if self.viewer is None:
-            from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
             bg = rendering.FilledPolygon([(0,0),(screen_width,0),(screen_width,screen_height),(0,screen_height),(0,0)])
             bg.set_color(0.2,0.2,0.2)
@@ -269,25 +141,22 @@ class FiveChessEnv(gym.Env):
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
 if __name__ == "__main__":
-    env = FiveChessEnv()
-    users = [-1,1]
-    agent_id = 0
+    env = FiveChessEnv(size=10,n_in_row=3)
     while True:
         env.reset()
         done = False
         while not done:
-            agent_id = (agent_id + 1) % 2
-            agent = users[agent_id]
             available_locations = env.get_available_locations()
             
             action = random.choice(available_locations)
-            action.append(agent)
 
-            _, reward, done, info = env.step(action)
+            _, reward, done, user = env.step(action)
             env.render(True)
 
             if done:
-                print(f'result={reward}')
-                time.sleep(3)
+                curr_user = env.get_current_player()
+                step_count = env.step_count
+                print(f'win_user: {user}, curr_user: {curr_user} reward: {reward} step_count: {step_count}')
+                time.sleep(30)
                 break
 
