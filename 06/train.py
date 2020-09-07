207259cba107d8e1cac81006df0d8c45c9cbe9ab
oneleaf <oneleaf@gmail.com>
2020-09-07 22:25:48 +0800

    .

--- 06/train.py
+++ 06/train.py
@@ -10,9 +10,9 @@ from collections import defaultdict, deque
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 size = 6  # 棋盘大小
-n_in_row = 5  # 几子连线
-model_file =  os.path.join(curr_dir, '../data/save/06_model_%s.pth'%size)
-best_model_file =  os.path.join(curr_dir, '../data/save/06_best_model_%s.pth'%size)
+n_in_row = 4  # 几子连线
+model_file =  os.path.join(curr_dir, '../data/save/06_model_%s_%s.pth'%(size,n_in_row))
+best_model_file =  os.path.join(curr_dir, '../data/save/06_best_model_%s_%s.pth'%(size,n_in_row))
 
 class FiveChessTrain():
     def __init__(self):
