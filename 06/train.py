a331afd0e02f2264bac214510456ac16b781fbe4
oneleaf <wp@bsw.com.cn>
2020-09-08 11:08:21 +0800

    .

--- 06/train.py
+++ 06/train.py
@@ -9,8 +9,8 @@ import numpy as np
 from collections import defaultdict, deque
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
-size = 8  # 棋盘大小
-n_in_row = 5  # 几子连线
+size = 6  # 棋盘大小
+n_in_row = 4  # 几子连线
 model_file =  os.path.join(curr_dir, '../data/save/06_model_%s_%s.pth'%(size,n_in_row))
 best_model_file =  os.path.join(curr_dir, '../data/save/06_best_model_%s_%s.pth'%(size,n_in_row))
 
