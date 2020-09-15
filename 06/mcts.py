a7797c5784ff361dfd2e4c7ae0bae9041aeeb5e9
oneleaf <wp@bsw.com.cn>
2020-09-15 15:48:14 +0800

    。

--- 06/mcts.py
+++ 06/mcts.py
@@ -250,9 +250,9 @@ class MCTS(object):
             state_copy = copy.deepcopy(state)
             self._playout_network(state_copy)
 
-            # 为了提高学习效率如果有走子都超过100次了，直接放弃再尝试返回。
+            # 为了提高学习效率如果有走子的此次超过目前探索次数的一半了，直接放弃再尝试返回。
             _n_visits = [node._n_visits for node in self._root._children.values()]
-            if max(_n_visits)>100:
+            if max(_n_visits)/sum(_n_visits)>0.5:
                 break
 
         # 分解出child中的action和最优选访问次数
