b6a0cd4396586eabe7275562ca915cdca9315a79
oneleaf <wp@bsw.com.cn>
2020-09-15 16:09:26 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -254,6 +254,7 @@ class MCTS(object):
             if n >= len(state.availables)*2:
                 _n_visits = [node._n_visits for node in self._root._children.values()]
                 if sum(heapq.nlargest(3,_n_visits))/sum(_n_visits)>0.5:
+                    print(_n_visits)
                     break
 
         # 分解出child中的action和最优选访问次数
