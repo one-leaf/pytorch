35fb6b319615a6e60cf22e3ac46a1f9115e72ddf
oneleaf <wp@bsw.com.cn>
2020-09-18 16:21:25 +0800

    .

--- 06/train.py
+++ 06/train.py
@@ -249,10 +249,10 @@ class FiveChessTrain():
                     self.policy_value_net.save_model(model_file)
                     # 收集自我对抗数据
                     logging.info("TRAIN Batch:{} starting, Size:{}, n_in_row:{}".format(step + 1, size, n_in_row))
-                    
+
                     lock = Lock()
                     p_list=[]
-                    for _ in self.play_batch_size:
+                    for _ in range(self.play_batch_size):
                         p = Process(target=self.collect_selfplay_data, args=(lock,))
                         p_list.append(p)
                         p.start()
