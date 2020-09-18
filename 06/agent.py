a27a7a566a2f5684968db7cf033dd34acfab50e6
oneleaf <wp@bsw.com.cn>
2020-09-18 17:00:13 +0800

    .

--- 06/agent.py
+++ 06/agent.py
@@ -7,12 +7,13 @@ class Agent(object):
         self.size = size
         self.n_in_row = n_in_row
         self.game = FiveChess(size=self.size, n_in_row=self.n_in_row)
+        self.is_shown = is_shown
         if is_shown:
             self.env = FiveChessEnv(self.game)
         self.game.reset()
 
     # 使用 mcts 训练，重用搜索树，并保存数据
-    def start_self_play(self, player, is_shown=0, temp=1e-3):
+    def start_self_play(self, player, temp=1e-3):
         """ start a self-play game using a MCTS player, reuse the search tree,
         and store the self-play data: (state, mcts_probs, z) for training
         """
@@ -22,6 +23,7 @@ class Agent(object):
         while True:
             # temp 权重 ，return_prob 是否返回概率数据
             action, move_probs = player.get_action(self.game, temp=temp, return_prob=1)
+            print(action)
             # store the data
             states.append(self.game.current_state())
             # print(action)
@@ -31,7 +33,7 @@ class Agent(object):
             current_players.append(self.game.current_player)
             # perform a move
             self.game.step(action)
-            if is_shown:
+            if self.is_shown:
                 self.env.render()
             end, winner = self.game.game_end()
             if end:
@@ -42,7 +44,7 @@ class Agent(object):
                     winners_z[np.array(current_players) != winner] = -1.0
                 # reset MCTS root node
                 player.reset_player()
-                if is_shown:
+                if self.is_shown:
                     if winner != -1:
                         print("Game end. Winner is player:", winner)
                     else:
@@ -50,7 +52,7 @@ class Agent(object):
                 return winner, zip(states, mcts_probs, winners_z)
 
     # AI和蒙特卡罗对战
-    def start_play(self, player1, player2, start_player=0, is_shown=1):
+    def start_play(self, player1, player2, start_player=0):
         """start a game between two players"""
         if start_player not in (0, 1):
             raise Exception('start_player should be either 0 (player1 first) '
@@ -60,18 +62,18 @@ class Agent(object):
         player1.set_player_ind(p1)
         player2.set_player_ind(p2)
         players = {p1: player1, p2: player2}
-        if is_shown:
+        if self.is_shown:
             self.env.render()
         while True:
             player_in_turn = players[self.game.current_player]
             action = player_in_turn.get_action(self.game)
             self.game.step(action)
 
-            if is_shown:
+            if self.is_shown:
                 self.env.render()
             end, winner = self.game.game_end()
             if end:
-                if is_shown:
+                if self.is_shown:
                     if winner != -1:
                         print("Game end. Winner is", players[winner])
                     else:
