aed15087edd8f9dbf40a4c59996bb748b5c56549
oneleaf <wp@bsw.com.cn>
2020-09-22 14:30:42 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -246,7 +246,7 @@ class MCTS(object):
             state_copy = copy.deepcopy(state)
             self._playout_network(state_copy)
 
-            # 为了提高学习效率如果有走子的方差大于100，直接放弃探索,返回。
+            # 为了提高学习效率如果有走子的方差大于10000，直接放弃探索,返回。
             if n%10==0 and n >= self._n_playout*0.2:
                 act_visits = [(act, node._n_visits) for act, node in self._root._children.items() if node._n_visits>0]
                 acts, visits = zip(*act_visits)
