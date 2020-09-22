c44eb99bdc31df5ad9ad654f5582cd0b7d5be219
oneleaf <oneleaf@gmail.com>
2020-09-22 22:30:34 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -276,11 +276,11 @@ class MCTS(object):
                     if var>10000:
                         break
             
-            # 如果得分为负数，并且不是双杀，就算10倍，争取找出一个优解
-            if n>self._n_playout:
-                idx = max(range(len(visits)), key=visits.__getitem__)
-                if self._root._children[acts[idx]].get_value(5)>0 or len(self._first_ations)>=2 or n>self._n_playout*10:
-                    break
+                # 如果得分为负数，并且不是双杀，就算10倍，争取找出一个优解
+                if n>=self._n_playout:
+                    idx = max(range(len(visits)), key=visits.__getitem__)
+                    if self._root._children[acts[idx]].get_value(5)>0 or len(self._first_ations)>=2 or n>self._n_playout*10:
+                        break
 
         # 分解出child中的action和最优选访问次数
         act_visits = [(act, node._n_visits) for act, node in self._root._children.items()]
