c0f954fcacc3b1a63282ccbcd3bf839e3914270c
oneleaf <wp@bsw.com.cn>
2020-09-24 11:02:10 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -294,7 +294,7 @@ class MCTS(object):
                     idx = max(range(len(visits)), key=visits.__getitem__)
                     # 如果当前的最佳选项在必救名单直接执行
                     if acts[idx] in self._first_ations:
-                        temp = 1e-3
+                        temp = 1e-5
                         break
 
                     # 如果得分为负数，并且不是双杀，就算10倍，争取找出一个优解
