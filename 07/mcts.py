73a4a287e957ebda48c6cc4e29d753c23654f6c9
oneleaf <wp@bsw.com.cn>
2020-09-27 19:07:37 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -250,15 +250,12 @@ class MCTS(object):
             self._playout_network(state_copy)
 
             # 为了提高学习效率如果有探索的标准差大于50，直接放弃探索,返回。
-            if n%10==0 and n >= self._n_playout*0.2:
+            if n%10==0 and n >= self._n_playout*0.1:
                 act_visits = [(act, node._n_visits) for act, node in self._root._children.items() if node._n_visits>0]
                 acts, visits = zip(*act_visits)
-
-                idx = max(range(len(visits)), key=visits.__getitem__)
-                if len(visits)>=2: 
-                    var = np.var(visits)
-                    if var>50**2:
-                        break
+                var = np.var(visits)
+                if var>100:
+                    break
             
                 if n>=self._n_playout:
                     # 如果得分为负数，多算2倍，争取找出一个优解
