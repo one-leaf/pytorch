07d0be36980814a53d70a39a3aad0c7a905c294f
oneleaf <oneleaf@gmail.com>
2020-09-28 08:59:08 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -275,7 +275,7 @@ class MCTS(object):
             value = self._root._children[acts[idx]].get_value(5)
             info[acts[idx]] = (visits[idx], round(value, 2))
 
-        state.print()
+        # state.print()
         print("_n_playout:", n, "info:", info)
         # softmax概率，先用log(visites)，拉平差异，再乘以一个权重，这样给了一个可以调节的参数，
         # temp 越小，导致softmax的越肯定，也就是当temp=1e-3时，基本上返回只有一个1,其余概率都是0; 训练的时候 temp=1
