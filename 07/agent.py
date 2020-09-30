4acea32f3033741ab0989e8c7b90984f2d8197f4
oneleaf <wp@bsw.com.cn>
2020-09-30 10:07:40 +0800

    .

--- 07/agent.py
+++ 07/agent.py
@@ -244,7 +244,8 @@ class Agent(object):
             self.badHoleCount = badHoleCount
         # if v>0: return 1.0
         # if v<0: return -1.0
-        if badHoleCount ==0: return 0
+        if badHoleCount ==0 and v==0: return 0
+        if badHoleCount ==0 and v>0: return v
         return (v/badHoleCount) * 10.0    
 
     def game_end(self):
