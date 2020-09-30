a82d1c389541e61c815b593b6e5b21777d59cb25
oneleaf <wp@bsw.com.cn>
2020-09-30 14:42:06 +0800

    .

--- 06/train.py
+++ 06/train.py
@@ -85,10 +85,10 @@ class FiveChessTrain():
         # training params
         self.learn_rate = 1e-5
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
-        self.temp = 1  # the temperature param
+        self.temp = 0.1  # the temperature param
         self.n_playout = 500  # 每个动作的模拟次数
         self.buffer_size = 100000  # cache对战记录个数
-        self.play_batch_size = 2 # 每次自学习次数
+        self.play_batch_size = 1 # 每次自学习次数
         self.epochs = 5  # 每次更新策略价值网络的训练步骤数, 推荐是5
         self.kl_targ = 0.02  # 策略价值网络KL值目标
         self.best_win_ratio = 0.0
