13d85572c13ec1e456bc583ad638704f8c3f7f96
oneleaf <oneleaf@gmail.com>
2020-10-01 11:20:20 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -183,7 +183,7 @@ class MCTS(object):
         # 尝试加一点点的修正
         if state.state==0:
             _ , new ,old = state.checkActionisBest(include_fallpiece=True)
-            leaf_value += (old-new)/old
+            leaf_value = (old-new)/old
         # 递归更新当前节点及所有父节点的最优选中次数和Q分数,因为得到的是本次的价值
         node.update_recursive(leaf_value)
 
