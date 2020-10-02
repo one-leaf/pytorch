0451be2bda7d88fb8358638a2881a31f4fafdb01
oneleaf <oneleaf@gmail.com>
2020-10-02 10:35:25 +0800

    .

--- 06/agent.py
+++ 06/agent.py
@@ -19,7 +19,7 @@ class Agent(object):
         """ start a self-play game using a MCTS player, reuse the search tree,
         and store the self-play data: (state, mcts_probs, z) for training
         """
-        self.game.reset()
+        self.game.reset(0 if random.random()>0.5 else 1)
         p1, p2 = self.game.players
 
         if not player2 is None:
