1e3dc00a3c90ef0b60ddca6c2a86fb43b5952983
oneleaf <wp@bsw.com.cn>
2020-10-13 09:42:50 +0800

    .

--- 07/train.py
+++ 07/train.py
@@ -95,7 +95,7 @@ class Train():
         self.n_playout = 64  # 每个动作的模拟次数
         self.buffer_size = 200000  # cache对战记录个数
         self.play_batch_size = 2 # 每次自学习次数
-        self.epochs = 1  # 每次更新策略价值网络的训练步骤数, 推荐是5
+        self.epochs = 5  # 每次更新策略价值网络的训练步骤数, 推荐是5
         self.kl_targ = 0.02  # 策略价值网络KL值目标
         self.best_win_ratio = 0.0
         
