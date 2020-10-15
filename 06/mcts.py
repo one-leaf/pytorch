7d0f2dff441fea335c650b6fc3724c7998982e76
oneleaf <wp@bsw.com.cn>
2020-10-15 11:49:11 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -321,7 +321,7 @@ class MCTS(object):
             info[acts[idx]] = (visits[idx], round(value, 2))
 
         temp = temp*((len(state.availables)/(state.size*state.size))**10)
-        print("player:",(state.step_count%2),"_n_playout:", n, "info:", info, "first:",self._first_ations)
+        print(state.step_count,"player:",(state.step_count%2),"_n_playout:", n, "info:", info, "first:",self._first_ations)
         # softmax概率，先用log(visites)，拉平差异，再乘以一个权重，这样给了一个可以调节的参数，
         # temp 越小，导致softmax的越肯定，也就是当temp=1e-3时，基本上返回只有一个1,其余概率都是0; 训练的时候 temp=1
         act_probs = MCTS.softmax((1/temp) * np.log(np.array(visits) + 1e-10))
