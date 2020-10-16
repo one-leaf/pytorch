a58b78d6890aae6a1b24ffb15e4d3cb28fce4c60
oneleaf <wp@bsw.com.cn>
2020-10-16 10:33:34 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -286,7 +286,7 @@ class MCTS(object):
             for idx in sorted(range(len(visits)), key=visits.__getitem__)[::-1]:
                 value = self._root._children[acts[idx]].get_value(5)
                 info[acts[idx]] = (visits[idx], round(value, 2))
-            # state.print(add_fallpiece=False)
+            state.print(add_fallpiece=False)
             # print(state.checkActionisBest(include_fallpiece=True))
             print("steps:",state.steps,"_n_playout:", n, "info:", info)
             # self.print_tree()
