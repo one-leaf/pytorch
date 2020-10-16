72d6a9f794de9851f575f1a141ae9f246c6a1202
oneleaf <wp@bsw.com.cn>
2020-10-16 17:40:54 +0800

    .

--- 07/agent.py
+++ 07/agent.py
@@ -465,6 +465,7 @@ class Agent(object):
         states0.extend(states1)
         mcts_probs0.extend(mcts_probs1)
         winner0.extend(winner1)
+        print(len(states0),len(mcts_probs0),len(winner0))
         assert len(states0)==len(mcts_probs0)==len(winner0)
         return -1, zip(states0, mcts_probs0, winner0)
 
