3eb5d54619d54caa45edbe39f7e8f11870a0d1d1
oneleaf <wp@bsw.com.cn>
2020-10-16 10:52:50 +0800

    。

--- 07/mcts.py
+++ 07/mcts.py
@@ -268,7 +268,7 @@ class MCTS(object):
                 # 如果只有一个选项，直接返回
                 if len(acts)==1: break
                 var = np.var(visits)
-                if var>100:
+                if var>1000:
                     break
             
                 # if n>=self._n_playout:
