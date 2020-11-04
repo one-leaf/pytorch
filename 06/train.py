b3e5a061b74338b3b37928ce61778dc2799f0086
oneleaf <wp@bsw.com.cn>
2020-11-04 22:42:00 +0800

    .

--- 06/train.py
+++ 06/train.py
@@ -271,7 +271,7 @@ class FiveChessTrain():
                     # 收集自我对抗数据
                     for _ in range(self.play_batch_size):
                         self.collect_selfplay_data()
-                    logging.info("TRAIN {} self-play end, size: {}".format(i, self.dataset.curr_size()))
+                    logging.info("TRAIN {} self-play end, size: {}".format(i, len(self.dataset)))
                    
             # 一轮训练完毕后与最佳模型进行对比
             self.policy_evaluate(self.policy_evaluate_size)
