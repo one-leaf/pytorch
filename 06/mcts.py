7e2cb4480ce379349832975b8f675978ee3e020d
oneleaf <wp@bsw.com.cn>
2020-11-24 19:04:52 +0800

    。

--- 06/mcts.py
+++ 06/mcts.py
@@ -197,15 +197,17 @@ class MCTS(object):
 
         # 检查游戏是否有赢家
         end, winner = state.game_end()
-        need_calc_leaf_value = (not end) and state.check_will_win() # 如果游戏没有结束，且没有将要结束
+        need_calc_leaf_value = end or state.check_will_win() # 如果游戏结束，或将要结束
 
-        if need_calc_leaf_value: 
-            # 没有结束或马上结束时，把走子策略返回的[(action,概率)]list加载到mcts树child中 ，同时降低了 leaf_value 的权重
+        leaf_value = 0.
+        if not end: 
+            # 没有结束时，把走子策略返回的[(action,概率)]list加载到mcts树child中 ，同时降低了 leaf_value 的权重
             # 使用训练好的模型策略评估此叶子节点，返回[(action,概率)]list 以及当前玩家的后续走子胜负
             action_probs, leaf_value = self._policy(state)
             node.expand(action_probs)
-        else:
-            # 游戏结束时返回真实的叶子胜负, 注意这里的游戏失败或成功的分值,由于后期还会重用搜索树，所以这里不能扩大奖励或惩罚分数
+
+        if need_calc_leaf_value:
+            # 游戏结束时或将结束时返回真实的叶子胜负, 注意这里的游戏失败或成功的分值,由于后期还会重用搜索树，所以这里不能扩大奖励或惩罚分数
             if winner == -1:  # tie平局
                 leaf_value = 0.0
             else:
