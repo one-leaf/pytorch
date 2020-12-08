6828273c06c61769016a7075bb4f96faf15db9c9
oneleaf <oneleaf@gmail.com>
2020-12-08 23:45:40 +0800

    。

--- 06/agent.py
+++ 06/agent.py
@@ -43,8 +43,8 @@ class Agent(object):
                 # mcts_probs.append(move_probs)
                 # current_players.append(self.game.current_player)   
                 # 如果包含了第二个玩家是MCTS，则AI每一步都需要重置搜索树
-                # if not player2 is None:
-                player_in_turn.mcts.update_root_with_action(None)             
+                if not player2 is None:
+                    player_in_turn.mcts.update_root_with_action(None)             
             else:
                 action, move_probs = player_in_turn.get_action(self.game, return_prob=1)
  
