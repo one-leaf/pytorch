929ea311f4614304ba35d3a2cbfd72703040e573
oneleaf <wp@bsw.com.cn>
2020-12-09 18:59:44 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -513,8 +513,8 @@ class MCTSPlayer(object):
                     action = random.choice(state.first_availables)
 
                 # 如果是下了几步后全部取最大值
-                # if state.step_count>=state.n_in_row*2:
-                if random.random() < state.step_count*2/(state.size*state.size):
+                if state.step_count>=state.n_in_row*4:
+                # if random.random() < state.step_count*2/(state.size*state.size):
                     action = acts[idx]
 
                 if action!=acts[idx]:
