dcb25e9c5dd679392fdd353da09b3c9bf2d1446c
oneleaf <wp@bsw.com.cn>
2020-12-09 09:59:29 +0800

    .

--- 06/selfplay.py
+++ 06/selfplay.py
@@ -84,7 +84,7 @@ class FiveChessTrain():
         self.learn_rate = 1e-4
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
         self.temp = 1  # 概率缩放程度，实际预测0.01，训练采用1
-        self.n_playout = 400  # 每个动作的模拟次数
+        self.n_playout = 300  # 每个动作的模拟次数
         self.play_batch_size = 1 # 每次自学习次数
         self.epochs = 1  # 重复训练次数, 推荐是5
         self.kl_targ = 0.02  # 策略价值网络KL值目标
