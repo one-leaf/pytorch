8d7606f5ea3e8d69b01e9343251d5846d7571e2a
oneleaf <wp@bsw.com.cn>
2020-12-16 13:36:35 +0800

    .

--- 06/playandtest.py
+++ 06/playandtest.py
@@ -141,7 +141,7 @@ class FiveChessPlay():
         best_mcts_player = MCTSPlayer(self.best_policy_value_net.policy_value_fn, c_puct=self.c_puct, n_playout=self.n_playout)
 
         agent = Agent(size, n_in_row, is_shown=0)
-        winner, play_data = agent.start_self_evaluate(current_mcts_player, best_mcts_player, temp=self.temp, start_player=i % 2)
+        winner, play_data = agent.start_self_evaluate(current_mcts_player, best_mcts_player, temp=self.temp, start_player=random.random()>0.5)
         if winner == current_mcts_player.player:
             self.best_win[0] = self.best_win[0]+1
             print("Curr Model Win!","win:", self.best_win[0],"lost",self.best_win[1])
@@ -172,7 +172,7 @@ class FiveChessPlay():
                     print("-"*50,"winner","-"*50)
                     print(winner)
                 self.policy_evaluate()
-                
+
                 if (i+1)%self.policy_evaluate_size == 0:
                     if self.mcts_win[0]>self.mcts_win[1]:                               
                         self.pure_mcts_playout_num=self.pure_mcts_playout_num+10
