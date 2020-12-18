a147c648953659b3ce420aee6f75bf84e9a6cc2e
oneleaf <wp@bsw.com.cn>
2020-12-18 19:02:38 +0800

    .

--- 06/agent.py
+++ 06/agent.py
@@ -36,8 +36,8 @@ class Agent(object):
             if isinstance(player_in_turn, MCTSPlayer):
                 action, move_probs = player_in_turn.get_action(self.game, temp=temp, return_prob=1)  
                 # 如果包含了第二个玩家是MCTS，则AI每一步都需要重置搜索树
-                if not player2 is None:
-                    player_in_turn.mcts.update_root_with_action(None)
+                # if not player2 is None:
+                #     player_in_turn.mcts.update_root_with_action(None)
             else:
                 action, move_probs = player_in_turn.get_action(self.game, return_prob=1)
  
