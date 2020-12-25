392da6c97594feec0d22fbb77f7a99989413af56
oneleaf <wp@bsw.com.cn>
2020-12-25 16:31:27 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -108,7 +108,7 @@ class MCTS():
                 # else:
                 v = -1
             elif state.is_defend():
-                v = -1
+                v = -0.5
                 # state.print()
             self.Es[s] = v
 
