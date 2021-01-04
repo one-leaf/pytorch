2068bee307108ec603dec4a7f02a5a0f2eadb295
oneleaf <wp@bsw.com.cn>
2021-01-04 19:23:47 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -117,7 +117,7 @@ class MCTS():
                 # if state.current_player==winner:
                 #     v = 1
                 # else:
-                v = -10
+                v = -1
             # 这个有问题，计算量有点大，放弃
             # elif state.check_will_win():
             #     v = -2
