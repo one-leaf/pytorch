67eb3268329933c50f2c7c58f5992256c74d63b8
oneleaf <wp@bsw.com.cn>
2021-03-13 15:36:29 +0800

    .

--- 06/mcts.py
+++ 06/mcts.py
@@ -317,7 +317,7 @@ class MCTSPlayer(object):
                     # 如果盘面看好，可以随机
                     # if act_qs[idx]>0 and state.step_count<state.n_in_row:
                     if act_qs[idx]<0.9: 
-                        p= 0.75                 
+                        p= 0.9                 
                         dirichlet = np.random.dirichlet(0.03 * np.ones(len(act_probs)))
                         act = np.random.choice(acts, p=p * act_probs + (1.0-p) * dirichlet)
                         action = state.position_to_action(act)
