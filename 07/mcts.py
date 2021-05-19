6ebdf1ba0704152aff104c8dcd0241973ade98d4
oneleaf <wp@bsw.com.cn>
2021-05-19 12:01:40 +0800

    。

--- 07/mcts.py
+++ 07/mcts.py
@@ -149,7 +149,7 @@ class MCTS():
             if state.state==1 and state.reward==0:
                 # print(state.curr_player)
                 if state.curr_player==0: 
-                    v = v-0.2               
+                    v = v-0.5               
 
             probs = np.zeros(state.actions_num)
             for act, prob in act_probs:
