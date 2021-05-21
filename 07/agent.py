8752a34fd45e5f2de4e955e77701e08a074cce21
oneleaf <wp@bsw.com.cn>
2021-05-21 11:54:42 +0800

    .

--- 07/agent.py
+++ 07/agent.py
@@ -410,10 +410,10 @@ class Agent(object):
             action, move_probs = player.get_action(game0, temp=temp, return_prob=1) 
             game0_states.append(game0.current_state())
             game0_players.append(game0.curr_player)
-            if game0.curr_player==0:
-                game0_mcts_probs.append(move_probs)
-            else:
-                game0_mcts_probs.append(np.ones(self.actions_num)/self.actions_num)
+            # if game0.curr_player==0:
+            game0_mcts_probs.append(move_probs)
+            # else:
+                # game0_mcts_probs.append(np.ones(self.actions_num)/self.actions_num)
 
             game0.step(action)
             if game0.terminal or game0.piececount>=train_pieces_count: break
@@ -431,11 +431,11 @@ class Agent(object):
             action, move_probs = player.get_action(game1, temp=temp, return_prob=1)
             game1_states.append(game1.current_state())
             game1_players.append(game1.curr_player)
-            if game1.curr_player==0:
-                game1_mcts_probs.append(move_probs)
-            else:
-                game1_mcts_probs.append(np.ones(self.actions_num)/self.actions_num)
-                
+            # if game1.curr_player==0:
+            game1_mcts_probs.append(move_probs)
+            # else:
+                # game1_mcts_probs.append(np.ones(self.actions_num)/self.actions_num)
+
             game1.step(action)
             if game1.terminal or game1.piececount>=train_pieces_count: break
 
