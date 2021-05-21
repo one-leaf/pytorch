56e0984611db7300617dc17e94a602fde5ef21e5
oneleaf <wp@bsw.com.cn>
2021-05-21 11:26:01 +0800

    .

--- 07/model.py
+++ 07/model.py
@@ -231,7 +231,7 @@ class PolicyValueNet():
         indices = torch.LongTensor([3]).to(self.device)
         mask = torch.index_select(state_batch,1,indices)  # 取出mask层 [batchsize, 1, 10, 20]
         mask = torch.mean(mask,[1,2,3])
-        print(mask.item())
+        print(mask.data.cpu().numpy())
         value_loss = F.mse_loss(value.view(-1), winner_batch)
         policy_loss = -torch.mean(torch.sum(mcts_probs * log_act_probs, 1))
         loss = value_loss + policy_loss
