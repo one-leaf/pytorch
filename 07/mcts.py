7490c412d673f2fbce1df4a6e9a38ffded55b0ea
oneleaf <wp@bsw.com.cn>
2021-05-24 16:48:21 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -334,9 +334,9 @@ class MCTSPlayer(object):
                 # print(np.std(act_probs))              
                 # if np.std(act_probs)<0.02 : 
                 # 早期多随机
-                p = 0.99                 
-                dirichlet = np.random.dirichlet(0.03 * np.ones(len(act_probs)))
-                act = np.random.choice(acts, p=p * act_probs + (1.0-p) * dirichlet)
+                #p = 0.99                 
+                #dirichlet = np.random.dirichlet(0.03 * np.ones(len(act_probs)))
+                #act = np.random.choice(acts, p=p * act_probs + (1.0-p) * dirichlet)
                 action = state.position_to_action(act)
 
                 if act!=acts[idx]:
