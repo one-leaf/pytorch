d00280455e21b010fa8898d37376c8f30982a933
oneleaf <wp@bsw.com.cn>
2021-05-31 23:00:42 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -133,7 +133,7 @@ class MCTS():
                 if state.state_player==0:
                     v = 10 - max_height
                 else:
-                    v =  max_height 
+                    v =  max_height - 10
 
             # 谁先落下来谁赢，这样由于下落的可能高，就倒逼正常走子远离下落
             # if state.state==1:
