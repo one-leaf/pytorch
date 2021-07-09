755557ac4e08f81e978655c2f63545ab07e45686
oneleaf <wp@bsw.com.cn>
2021-07-09 15:41:31 +0800

    .

--- 07/agent.py
+++ 07/agent.py
@@ -537,29 +537,32 @@ class Agent(object):
             else:
                 game0_win = -1
                 game1_win = 1
-
-        if game0_win == -1 and game1_win==0:
+        else:
+            game0_win = -1 
             game1_win = 1
-        if game1_win == -1 and game0_win==0:
-            game0_win = 1
 
-        if game1_win==0 and game0_win==0:
-            game0_exscore = -1 * game0.getTransCount()
-            game1_exscore = -1 * game1.getTransCount()
+        # if game0_win == -1 and game1_win==0:
+        #     game1_win = 1
+        # if game1_win == -1 and game0_win==0:
+        #     game0_win = 1
+
+        # if game1_win==0 and game0_win==0:
+        #     game0_exscore = -1 * game0.getTransCount()
+        #     game1_exscore = -1 * game1.getTransCount()
                 
-            print("game0_exscore:",game0_exscore,"game1_exscore:",game1_exscore)
+        #     print("game0_exscore:",game0_exscore,"game1_exscore:",game1_exscore)
 
-            if game0_exscore>game1_exscore:
-                game0_win = 1
-                game1_win = -1
+        #     if game0_exscore>game1_exscore:
+        #         game0_win = 1
+        #         game1_win = -1
 
-            if game0_exscore<game1_exscore:
-                game0_win = -1
-                game1_win = 1
+        #     if game0_exscore<game1_exscore:
+        #         game0_win = -1
+        #         game1_win = 1
 
-            if game0_exscore==game1_exscore:
-                game0_win = -1
-                game1_win = -1
+        #     if game0_exscore==game1_exscore:
+        #         game0_win = -1
+        #         game1_win = -1
 
         print("game0",game0_win,"game1",game1_win,"")
         winers, mask = [],[]
