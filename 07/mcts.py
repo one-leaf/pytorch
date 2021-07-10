e2756983c4bd6baef45454ab63dca52c38e4091a
oneleaf <wp@bsw.com.cn>
2021-07-10 10:30:35 +0800

    .

--- 07/mcts.py
+++ 07/mcts.py
@@ -309,7 +309,7 @@ class MCTSPlayer(object):
                 # if np.std(act_probs)<0.02 : 
                 # 早期多随机
                 #if act_probs[idx]<0.99:
-                p = 0.5                 
+                p = 0.75                 
                 dirichlet = np.random.dirichlet(0.03 * np.ones(len(act_probs)))
                 act = np.random.choice(acts, p=p * act_probs + (1.0-p) * dirichlet)
                
