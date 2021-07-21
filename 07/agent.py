403ff5001ddeb268743c82343efffadeda129dfa
oneleaf <wp@bsw.com.cn>
2021-07-21 18:38:28 +0800

    。

--- 07/agent.py
+++ 07/agent.py
@@ -539,6 +539,7 @@ class Agent(object):
         # 检查谁下的好
         p0, p1 = game0.piececount, game1.piececount
 
+        # 低于消除2行的全部为输，至少要消除2行才做判定
         if game0.score>=2 or game1.score>=2:
             if game0.score>game1.score:
                 game0_win = 1
