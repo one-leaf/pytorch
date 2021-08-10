5c07223427bbb4b87a3f344a780a39adc33444f9
oneleaf <wp@bsw.com.cn>
2021-08-10 11:27:41 +0800

    .

--- 09/agent.py
+++ 09/agent.py
@@ -764,7 +764,7 @@ class Agent(object):
             self.limit_max_height=random.randint(5,12)
             self.ig_action=random.choice([None,KEY_NONE,KEY_DOWN])
 
-        print("limit_max_height:", self.limit_max_height, "ig_action:", self.ig_action)
+        print("limit_max_height:", self.limit_max_height, "ig_action:", ACTIONS_NAME[self.ig_action])
         states, mcts_probs, current_players = [], [], []
 
         # # 先随机走1~2步，增加样本的复杂度
