80d475455ebad760a7b2bf31bc73ac01abe8452a
oneleaf <wp@bsw.com.cn>
2021-08-13 15:16:10 +0800

    。

--- 09/train.py
+++ 09/train.py
@@ -99,7 +99,7 @@ class Dataset(torch.utils.data.Dataset):
             os.rename(os.path.join(data_wait_dir,fn), savefile)
             self.index += 1
             self.save_index() 
-            if i>=200: break       
+            if i>=100: break       
         print("mv %s/%s files to train"%(i,len(movefiles)))
         if i==0:
             print("SLEEP 60s for watting data")
@@ -129,7 +129,7 @@ class Train():
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
         self.n_playout = 64  # 每个动作的模拟战记录个数
         self.play_batch_size = 1 # 每次自学习次数
-        self.buffer_size = 500000  # cache对次数
+        self.buffer_size = 300000  # cache对次数
         self.epochs = 1  # 每次更新策略价值网络的训练步骤数, 推荐是5
         self.kl_targ = 0.02  # 策略价值网络KL值目标
         self.best_win_ratio = 0.0
