ee67abb756ab88e7b87be09f57b2a772ad13d22d
oneleaf <wp@bsw.com.cn>
2021-08-17 14:13:03 +0800

    .

--- 09/train.py
+++ 09/train.py
@@ -62,7 +62,7 @@ class Dataset(torch.utils.data.Dataset):
                 os.remove(filename)
                 filename = random.choice(self.file_list)
             else:
-                if abs(self.sample+winner)<1000:
+                if abs(self.sample+winner)<10000:
                     self.sample = self.sample + winner
                     break
                 else:
