9aa22accde63287b630ae2d1baefad47b64bd75c
oneleaf <wp@bsw.com.cn>
2021-09-04 15:19:46 +0800

    .

--- 09/selfplay.py
+++ 09/selfplay.py
@@ -97,7 +97,7 @@ class Train():
         self.learn_rate = 1e-4
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
-        self.n_playout = 256  # 每个动作的模拟战记录个数
+        self.n_playout = 500  # 每个动作的模拟战记录个数
         self.play_batch_size = 5 # 每次自学习次数
         self.buffer_size = 500000  # cache对次数
         self.epochs = 2  # 每次更新策略价值网络的训练步骤数, 推荐是5
