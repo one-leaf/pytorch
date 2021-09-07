a86adbb977351c64bf5d5cd111609fd6f7bba03c
oneleaf <wp@bsw.com.cn>
2021-09-07 12:01:09 +0800

    .

--- 09/agent.py
+++ 09/agent.py
@@ -383,39 +383,6 @@ class Agent(object):
             game_player_0[j] = 1 if game_winer[j]==0 else -1
             game_player_1[j] = -1 * game_player_0[j]
 
-
-        # 如果只有一个最大值
-        # if game_piececount.count(max_piececount)==1:
-        #     for j in range(game_num):
-        #         if game_piececount[j]==max_piececount:
-        #             game_player_0[j] = 1
-        #             game_player_1[j] = 1
-        #         else:
-        #             game_player_0[j] = -1
-        #             game_player_1[j] = -1
-        # else:
-        #     for j in range(game_num):
-        #         game_player_0[j] = 1 if game_winer[j]==0 else -1
-        #         game_player_1[j] = -1 * game_player_0[j]
-
-        # for j in range(game_num):
-        #     # if max_score>0 and game_score[j]==max_score:
-        #     #     game_player_0[j] = 1
-        #     #     game_player_1[j] = 1
-        #     # else:
-        #     #     game_player_0[j] = 1 if game_winer[j]==0 else -1
-        #     #     game_player_1[j] = -1 * game_player_0[j]
-
-        #     if game_piececount[j]==max_piececount:
-        #         game_player_0[j] = 1
-        #         game_player_1[j] = 1
-
-        #     # elif max_score>0 and game_score[j]==max_score:
-        #     #     game_player_0[j] = 1
-        #     #     game_player_1[j] = 1
-        #     # else:
-        #     #     game_player_0[j] = -1
-        #     #     game_player_1[j] = -1
         print("game_piececount",game_piececount,"game_score",game_score)
         print("game_player_0",game_player_0,"game_player_1",game_player_1)
 
