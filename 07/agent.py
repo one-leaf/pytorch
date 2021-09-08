3abfabed8f0e0ce2208256d412230b656f434ab8
oneleaf <wp@bsw.com.cn>
2021-09-08 18:51:04 +0800

    .

--- 07/agent.py
+++ 07/agent.py
@@ -454,7 +454,11 @@ class Agent(object):
             self.limit_max_height = limit_max_height
 
         # 玩几局订胜负，如果最高为5，玩4局，否则玩2局
-        game_num = 4 if limit_max_height != 10 else 2
+        if limit_max_height != 10:
+            game_num = 4
+            player.mcts._n_playout = 32
+        else:
+            game_num = 2
         
         game_states, game_mcts_probs, game_masks = [],[],[] 
         game_piececount, game_score = [],[]
