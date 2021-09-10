04c55e0753327df20bde9f6cc8e475bb0f0edb4d
oneleaf <wp@bsw.com.cn>
2021-09-10 23:19:48 +0800

    .

--- 09/agent.py
+++ 09/agent.py
@@ -327,10 +327,10 @@ class Agent(object):
         # print("limit_max_height:", limit_max_height)
 
         game_num = 5
-        if random.random()>0.2:
+        if random.random()>0.8:
             self.limit_max_height = 10
         else:
-            self.limit_max_height = 20
+            self.limit_max_height = random.randint(5,20)
         limit_max_height = self.limit_max_height
         game_states, game_mcts_probs, game_current_players = [],[],[] 
         game_piececount, game_score, game_winer = [],[],[]
