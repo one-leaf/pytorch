31a039872b9e4c3a2b30f08a9e2ffb1039cbb75d
oneleaf <wp@bsw.com.cn>
2021-09-10 17:55:04 +0800

    .

--- 09/mcts.py
+++ 09/mcts.py
@@ -317,8 +317,8 @@ class MCTSPlayer(object):
                 # if act in [0,4] and random.random()>0.5:
                 # if act_probs[idx]<0.99:
                 # if abs(value)>0.5 or random.random()>0.95:
-                if state.piececount < 50 and random.random()>0.5:
-                    p = 0.99                 
+                if state.piececount < 50 and state.piecesteps<5:
+                    p = 0.75                 
                     dirichlet = np.random.dirichlet(0.03 * np.ones(len(act_probs)))
                     act = np.random.choice(acts, p=p * act_probs + (1.0-p) * dirichlet)
                 action = state.position_to_action(act)
