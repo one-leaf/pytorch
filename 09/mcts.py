a9a2c2be797db28d584d3097801a8279d8c3b35d
oneleaf <wp@bsw.com.cn>
2021-09-10 10:45:21 +0800

    .

--- 09/mcts.py
+++ 09/mcts.py
@@ -124,17 +124,18 @@ class MCTS():
                 else:
                     v = -1 
                 # v = 1
-            elif state.state == 1:
-                if state.reward>0:
-                    return -1 * -1
-                elif random.random()>0.5:
-                    return -1 * 1
             self.Es[s] = v
 
         # 如果得分不等于0，标志这局游戏结束
         if self.Es[s] != 0 or state.terminal:
             # print("v", self.Es[s], "reward", state.reward, "curr_player", state.curr_player, "ph", state.pieces_height)
             return -self.Es[s]
+
+        if state.state == 1:
+            if state.reward>0:
+                return -1 * -1
+            elif random.random()>0.5:
+                return -1 * 1
 
         # 如果当前状态没有子节点，增加子节点
         # 增加 Ps[s] Vs[s] Ns[s]
