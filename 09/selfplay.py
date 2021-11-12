04a8665024b4d894bb3fc034721b02b6b2877ce0
oneleaf <wp@bsw.com.cn>
2021-11-12 10:05:28 +0800

    .

--- 09/selfplay.py
+++ 09/selfplay.py
@@ -173,9 +173,9 @@ class Train():
         result[limit_max_height]["curr"]["agent"] += agentcount
 
         agent = result[limit_max_height]["agent"]
-        if agent%200==0:
-            result[limit_max_height]["reward"].append(result[limit_max_height]["curr"]["reward"]/result[limit_max_height]["curr"]["agent"])
-            result[limit_max_height]["pieces"].append(result[limit_max_height]["curr"]["pieces"]/result[limit_max_height]["curr"]["agent"])
+        if agent%100==0:
+            result[limit_max_height]["reward"].append(round(result[limit_max_height]["curr"]["reward"]/result[limit_max_height]["curr"]["agent"]),2)
+            result[limit_max_height]["pieces"].append(round(result[limit_max_height]["curr"]["pieces"]/result[limit_max_height]["curr"]["agent"]),2)
         if agent%100==1000:
             result[limit_max_height]["curr"]={"reward":0,"pieces":0,"agent":0}
 
