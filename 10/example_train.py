9fc5afcdb700518a68c94b4e07df4b68da19b39f
oneleaf <wp@bsw.com.cn>
2021-11-19 10:21:31 +0800

    .

--- 10/example_train.py
+++ 10/example_train.py
@@ -9,7 +9,7 @@ import os
 import glob
 
 # Decide which device we want to run on
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 if __name__ == '__main__':
