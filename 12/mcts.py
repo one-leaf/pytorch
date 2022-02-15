80715a610bb665b02c75adbb0d7b8566b067d74b
oneleaf <oneleaf@gmail.com>
2022-02-15 11:06:03 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -203,7 +203,7 @@ class MCTSPlayer(object):
             max_idx = np.argmax(act_probs)    
 
             if need_random:  # 自我对抗
-                if np.random.random() < 0.05:
+                if np.random.random() < 0.01:
                     idx = np.random.randint(len(acts))
                 else:
                     idx = np.random.choice(range(len(acts)), p=act_probs)
