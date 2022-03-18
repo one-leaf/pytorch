84c13174f98a9bdb4c624251c8c05356e155a684
oneleaf <oneleaf@gmail.com>
2022-03-18 16:37:44 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -152,10 +152,10 @@ class MCTS():
         # 后期训练不需要，只是用于前期引导
         if state.state == 1:
             if state.reward>0: 
-                v = self.search(state)+0.1
+                v = self.search(state)+0.01
                 if v>1: v=1
             else:
-                v = self.search(state)-0.1
+                v = self.search(state)-0.01
                 if v<-1: v=-1
         else:
             v = self.search(state)
