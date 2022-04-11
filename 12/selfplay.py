42a4301d23ef91e57e2bac4d6eb80c78dd004357
oneleaf <oneleaf@gmail.com>
2022-04-11 18:00:27 +0800

    .

--- 12/selfplay.py
+++ 12/selfplay.py
@@ -128,7 +128,7 @@ class Train():
                     step_reward = reward #* 10
                     _rewards.append(step_reward)
                     for k in range(len(_rewards)-2, -1, -1):
-                        step_reward = step_reward*0.95 
+                        step_reward = step_reward*0.999 
                         _rewards[k] += step_reward
 
                     print("#"*40, 'score:', game.score, 'height:', game.pieceheight, 'piece:', game.piececount, 'step:', i, "#"*40)
