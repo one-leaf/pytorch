ca3c2b115e2b2e8dc5d242257f6680ba61b18afa
oneleaf <oneleaf@gmail.com>
2022-05-14 11:04:29 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -84,8 +84,8 @@ class MCTS():
         # m = np.power(np.array(visits), 1./temp)
         m = np.array(visits)
         m_sum = np.sum(m)
-        if m_sum<=1:
-            v_len = len(acts)
+        v_len = len(acts)
+        if m_sum<=v_len:
             probs = np.ones(v_len)/v_len
         else:
             probs = m/m_sum
