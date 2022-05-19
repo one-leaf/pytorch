b9757b6b3566535281d55feb6893015a0ab27bed
oneleaf <oneleaf@gmail.com>
2022-05-19 17:12:22 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -225,7 +225,7 @@ class MCTSPlayer(object):
                 # elif random.random()>max_height/10 :
                 if state.get_key() in game_keys:
                     idx = np.random.randint(len(acts))
-                    print(state.steps, state.piececount, state.fallpiece["shape"], state.piecesteps, state.get_key(), len(game_keys))
+                    print(state.steps, state.piececount, state.fallpiece["shape"], state.piecesteps, "key:", state.get_key(), "key_len:" ,len(game_keys))
                     #idx = np.random.choice(range(len(acts)), p=act_probs)
                 else:
                     idx = np.random.choice(range(len(acts)), p=act_probs)
