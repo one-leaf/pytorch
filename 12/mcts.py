c1d02b5fa53964717593521f875371be2472b367
oneleaf <oneleaf@gmail.com>
2022-06-16 14:08:56 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -51,7 +51,7 @@ class MCTS():
             self.search(state_copy)
             if self.depth>self.max_depth: self.max_depth = self.depth
             visits_sum = sum([self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in available_acts])          
-            if visits_sum > self._n_playout : break
+            if visits_sum >= self._n_playout : break
             # 计算所有动作的探索次数，如果大于2000，则中断
             # if n >= self._n_playout*5 or visits_sum > 2000: break
             # if n >= self._n_playout and visits_sum >= 1000: break
