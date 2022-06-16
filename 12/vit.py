f4d7801276baa3fbe92d060b5c1fa6b25035751f
oneleaf <oneleaf@gmail.com>
2022-06-16 17:50:02 +0800

    .

--- 12/vit.py
+++ 12/vit.py
@@ -310,7 +310,7 @@ class VisionTransformer(nn.Module):
 
 
 class VitNet(nn.Module):
-    def __init__(self,  embed_dim=768, drop_ratio=0.25, drop_path_ratio=0.5, depth=12, num_heads=12, 
+    def __init__(self,  embed_dim=768, drop_ratio=0.25, drop_path_ratio=0.25, depth=12, num_heads=12, 
                         mlp_ratio=4.0, qkv_bias=True, qk_scale=None, attn_drop_ratio=0., num_classes=1000):
         super(VitNet, self).__init__()
 
