70a3cc65a267661902bc076e158ef92fb9d462f2
oneleaf <oneleaf@gmail.com>
2022-07-18 11:24:15 +0800

    .

--- 12/selfplay.py
+++ 12/selfplay.py
@@ -24,7 +24,7 @@ class Train():
         self.learn_rate = 1e-4
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
-        self.n_playout = 600  # 每个动作的模拟战记录个数，不能用32，因为收敛太慢了
+        self.n_playout = 500  # 每个动作的模拟战记录个数，不能用32，因为收敛太慢了
         self.play_batch_size = 5 # 每次自学习次数
         self.buffer_size = 1000000  # cache对次数
         self.epochs = 2  # 每次更新策略价值网络的训练步骤数, 推荐是5
@@ -295,15 +295,16 @@ class Train():
                 data["steps"][i]["reward"] = v
             print(vlist)
 
+        # 未来的变数太多，所以下面只能放弃
         # 总得分为 局部奖励  + (总奖励-平均奖励/平均奖励)
-        for data in game_datas:
-            step_count = len(data["steps"])
-            weight = (data["score"]-result["QVal"])/result["QVal"]
-            for i in range(step_count):
-                if data["steps"][i]["reward"] < 1:
-                    v = data["steps"][i]["reward"] + weight 
-                    if v>1: v=1
-                    data["steps"][i]["reward"] = v
+        # for data in game_datas:
+        #     step_count = len(data["steps"])
+        #     weight = (data["score"]-result["QVal"])/result["QVal"]
+        #     for i in range(step_count):
+        #         if data["steps"][i]["reward"] < 1:
+        #             v = data["steps"][i]["reward"] + weight 
+        #             if v>1: v=1
+        #             data["steps"][i]["reward"] = v
         
         print("fixed reward")
         for data in game_datas:
