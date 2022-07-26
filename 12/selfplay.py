77a4248c025dd3fa2d824a649cb64948ae7b7116
oneleaf <oneleaf@gmail.com>
2022-07-26 20:16:32 +0800

    .

--- 12/selfplay.py
+++ 12/selfplay.py
@@ -283,7 +283,7 @@ class Train():
             print(line)
         print((" "+" -"*agent.width+" ")*len(borads))
 
-        ## 放弃 按0.99的衰减更新reward
+        ## 放弃 按0.95的衰减更新reward
         # 只关注最后一次得分方块的所有步骤,将消行方块的所有步骤的得分都设置为1
         for data in game_datas:
             step_count = len(data["steps"])
@@ -291,7 +291,7 @@ class Train():
             v = 0
             vlist=[]
             for i in range(step_count-1,-1,-1):
-                v = 0.99*v+data["steps"][i]["reward"]
+                v = 0.95*v+data["steps"][i]["reward"]
                 v = math.tanh(v)
                 if piece_count!=data["steps"][i]["piece_count"]:
                     piece_count = data["steps"][i]["piece_count"]
