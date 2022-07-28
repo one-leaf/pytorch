f4c377384f2edf99d277662d010f698481f8ef28
oneleaf <oneleaf@gmail.com>
2022-07-28 18:19:32 +0800

    .

--- 12/train.py
+++ 12/train.py
@@ -84,7 +84,7 @@ class Dataset(torch.utils.data.Dataset):
         avg_score = sum(scores)/len(scores)
         values = [] 
         for fn in self.data:
-            self.data[fn]["value"] = self.data[fn]["value"]*0.5 + math.tanh(self.data[fn]["score"]/avg_score)*0.5
+            self.data[fn]["value"] = self.data[fn]["value"]*0.4 + math.tanh(self.data[fn]["score"]/avg_score)*0.6
             values.append(self.data[fn]["value"])
         curr_avg_value = sum(values)/len(values)
         curr_std_value = np.std(values)+1e-8
