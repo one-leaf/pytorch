a0fc8a6acf72eae5ff51a12903a4083dfc71d6dc
oneleaf <oneleaf@gmail.com>
2022-08-02 09:58:50 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -44,17 +44,17 @@ class MCTS():
         self.state = state
         self.max_depth = 0
         available_acts = state.actions_to_positions(state.availables)
-        for n in range(self._n_playout):
-        # for n in count():
+        # for n in range(self._n_playout):
+        for n in count():
             self.depth = 0
             state_copy = copy.deepcopy(state)
-            self.search(state_copy)
+            v = self.search(state_copy)
             if self.depth>self.max_depth: self.max_depth = self.depth
             # visits_sum = sum([self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in available_acts])          
             # if visits_sum >= self._n_playout or n > 2000: break
             # 计算所有动作的探索次数，如果大于2000，则中断
-            # if n >= self._n_playout*5 or visits_sum > 2000: break
-            # if n >= self._n_playout and visits_sum >= 1000: break
+            if n >= self._n_playout*4 : break
+            if n >= self._n_playout and v >= -0.5: break
 
         act_visits = [(a, self.Nsa[(s, a)]) if (s, a) in self.Nsa else (a, 0) for a in available_acts]
         act_Qs = [(a, self.Qsa[(s, a)]) if (s, a) in self.Qsa else (a, 0) for a in available_acts]
