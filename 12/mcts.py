ff9093e47959901f7b5873cb4d5d658c230dd917
oneleaf <oneleaf@gmail.com>
2022-08-03 11:32:33 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -53,7 +53,7 @@ class MCTS():
             # visits_sum = sum([self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in available_acts])          
             # if visits_sum >= self._n_playout or n > 2000: break
             # 计算所有动作的探索次数，如果大于2000，则中断
-            if n >= self._n_playout*10 : break
+            if n >= self._n_playout*40 : break
             if n >= self._n_playout and v >= -0.5: break
 
         act_visits = [(a, self.Nsa[(s, a)]) if (s, a) in self.Nsa else (a, 0) for a in available_acts]
