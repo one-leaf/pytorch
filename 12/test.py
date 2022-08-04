a99e3e037fecbba3dd6fe22ed2d134c4fe288ae6
oneleaf <oneleaf@gmail.com>
2022-08-04 20:03:13 +0800

    .

--- 12/test.py
+++ 12/test.py
@@ -28,12 +28,12 @@ def run():
             
             agent.step(act)
             # print(agent.get_availables())
-            # os.system("cls")
+            os.system("cls")
             print(act_probs, v, agent.position_to_action_name(act))
-            # agent.print2()
+            agent.print2()
             time.sleep(0.1)
-            print(agent.current_state())
-            input()
+            # print(agent.current_state())
+            # input()
     except KeyboardInterrupt:
         print('quit')
 
