6c1800a47acbd5e360708e0d3e752bf78a2d0be6
oneleaf <oneleaf@gmail.com>
2022-08-09 15:39:15 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -256,7 +256,7 @@ class MCTSPlayer(object):
 
             if state.show_mcts_process:
                 if idx!=max_idx:
-                    print("    random:", state.position_to_action_name(acts[max_idx]), "==>", state.position_to_action_name(action))  
+                    print("    random:", state.position_to_action_name(acts[max_idx]), act_probs[max_idx], "==>", state.position_to_action_name(acts[idx]), act_probs[idx])  
 
 
             # print(acts, act_probs, idx, action)
