696abe998e624443697eaf707d08b88798f1e595
oneleaf <oneleaf@gmail.com>
2022-08-15 17:12:56 +0800

    .

--- 12/mcts.py
+++ 12/mcts.py
@@ -211,7 +211,7 @@ class MCTSPlayer(object):
             move_probs[acts] = act_probs
             max_idx = np.argmax(act_probs)    
 
-            if temp==0:
+            if temp==0 or act_qs[max_idx]>0:
                 idx = max_idx
             else:
                 p = 0.95                 
