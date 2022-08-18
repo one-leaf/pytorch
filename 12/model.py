6655a7b32e434fd07a339261aa88c09c2d4a3d3d
oneleaf <oneleaf@gmail.com>
2022-08-18 16:17:50 +0800

    .

--- 12/model.py
+++ 12/model.py
@@ -252,26 +252,24 @@ class PolicyValueNet():
         输入: 游戏
         输出: 一组（动作， 概率）和游戏当前状态的胜率
         """
-        # key = game.get_key()
-        # if key in self.cache:
-        #     act_probs, value = self.cache[key] 
-        # else:
-        if self.load_model_file:
-            current_state = game.current_state().reshape(1, -1, self.input_height, self.input_width)
-            act_probs, value = self.policy_value(current_state)
-            act_probs = act_probs.flatten()
+        key = game.get_key()
+        if key in self.cache:
+            act_probs, value = self.cache[key] 
         else:
-            act_len=game.actions_num
-            act_probs=np.ones([act_len])/act_len
-            value = np.array([[0.]])
-        
-        actions = game.availables
-        act_probs = list(zip(actions, act_probs[actions]))
+            if self.load_model_file:
+                current_state = game.current_state().reshape(1, -1, self.input_height, self.input_width)
+                act_probs, value = self.policy_value(current_state)
+                act_probs = act_probs.flatten()
+            else:
+                act_len=game.actions_num
+                act_probs=np.ones([act_len])/act_len
+                value = np.array([[0.]])
+            
+            actions = game.availables
+            act_probs = list(zip(actions, act_probs[actions]))
 
-        value = value[0,0]
-        # 做一个侵入性的改变
-        # if game.reward>0: value = 1.0
-        # self.cache[key] = (act_probs, value)
+            value = value[0,0]
+            self.cache[key] = (act_probs, value)
         return act_probs, value
 
 
