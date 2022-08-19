274be6f695838f7930e65405175c63e7e1ba18b2
oneleaf <oneleaf@gmail.com>
2022-08-19 18:54:01 +0800

    .

--- 12/selfplay.py
+++ 12/selfplay.py
@@ -114,7 +114,7 @@ class Train():
         print("TRAIN Self Play starting ...")
 
         # 游戏代理
-        agent = Agent(smax_height=5)
+        agent = Agent(max_height=5)
 
         borads = []
         game_datas = []
