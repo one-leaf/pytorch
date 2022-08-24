ad4c323f39c9a6c35771213962eb9552e80dc815
oneleaf <oneleaf@gmail.com>
2022-08-24 11:08:52 +0800

    .

--- 12/train.py
+++ 12/train.py
@@ -243,7 +243,7 @@ class Train():
                 test_batch, test_probs, test_values = data
                 test_batch.to(self.policy_value_net.device)
                 with torch.no_grad(): 
-                    act_probs, value = self.policy_value_net.forward(test_batch)                
+                    act_probs, value = self.policy_value_net.policy_value_net.forward(test_batch)                
                     test_values.to(self.policy_value_net.device)
                     loss = loss_fn(value, test_values.unsqueeze(-1))
                     print("test value loss", loss)
