91ae1efdbd89e01eca1a1bb514b48d8c52326c7d
oneleaf <oneleaf@gmail.com>
2022-08-24 22:52:54 +0800

    .

--- 12/train.py
+++ 12/train.py
@@ -214,19 +214,19 @@ class Train():
 
                     # 动态调整学习率
                     if old_probs is None:
-                        test_batch, test_probs, test_values = state, mcts_prob, value
+                        test_batch, test_probs, test_values = data
                         old_probs, old_value = self.policy_value_net.policy_value(test_batch) 
                     else:
                         new_probs, new_value = self.policy_value_net.policy_value(test_batch)
                         kl = np.mean(np.sum(old_probs * (np.log(old_probs + 1e-10) - np.log(new_probs + 1e-10)), axis=1))
                         
-                        if i % 50 == 0:   
-                            print("probs[0] old:{}".format(old_probs[0]))   
-                            print("probs[0] new:{}".format(new_probs[0]))
-                            print("probs[0] dst:{}".format(test_probs[0]))   
-                            maxlen = min(10, len(test_batch)) 
-                            for j in range(maxlen): 
-                                print("value[0] old:{} new:{} tg:{}".format(old_value[j][0], new_value[j][0], test_values[j]))  
+                        # if i % 50 == 0:   
+                        #     print("probs[0] old:{}".format(old_probs[0]))   
+                        #     print("probs[0] new:{}".format(new_probs[0]))
+                        #     print("probs[0] dst:{}".format(test_probs[0]))   
+                        #     maxlen = min(10, len(test_batch)) 
+                        #     for j in range(maxlen): 
+                        #         print("value[0] old:{} new:{} tg:{}".format(old_value[j][0], new_value[j][0], test_values[j]))  
 
                         old_probs = None
                         
