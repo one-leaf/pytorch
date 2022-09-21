8e4b5ace8921a0a17177a220dc05e80c0f6cc636
oneleaf <oneleaf@gmail.com>
2022-09-21 22:55:03 +0800

    .

--- 15/selfplay.py
+++ 15/selfplay.py
@@ -111,7 +111,7 @@ class Train():
         print("TRAIN Self Play starting ...")
 
         # 游戏代理
-        agent = Agent(max_height=5)
+        agent = Agent(max_height=5, isRandomNextPiece=True)
 
         borads = []
 
@@ -123,7 +123,7 @@ class Train():
         # if random.random()>0.9:
         #     agent2 = copy.deepcopy(agent)
         # else:
-        agent2 = Agent(max_height=5)
+        agent2 = Agent(max_height=5, isRandomNextPiece=True)
         games = (agent, agent2)
 
         agent.show_mcts_process= True
