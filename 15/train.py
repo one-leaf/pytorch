4c982ee2f96fa2a082b432bf90836a5cf629a20a
oneleaf <oneleaf@gmail.com>
2022-09-24 15:49:14 +0800

    .

--- 15/train.py
+++ 15/train.py
@@ -97,7 +97,8 @@ class Dataset(torch.utils.data.Dataset):
                 self.file_list.remove(fn)
                 continue
             # 这里准备数据的时候直接 value = (value+qval)/2 同时考虑两者
-            self.data[fn]={"value":(value+qval)/2., "state":state, "mcts_prob": mcts_prob}
+            # self.data[fn]={"value":(value+qval)/2., "state":state, "mcts_prob": mcts_prob}
+            self.data[fn]={"value":qval, "state":state, "mcts_prob": mcts_prob}
             sum_v+=value
             
         # 将qval高的重复学习一次    
