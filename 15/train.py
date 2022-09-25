e4f4d4c2fbeda5e741f664f6975e90df711ee328
oneleaf <oneleaf@gmail.com>
2022-09-25 23:47:52 +0800

    .

--- 15/train.py
+++ 15/train.py
@@ -98,8 +98,8 @@ class Dataset(torch.utils.data.Dataset):
                 continue
             # 这里准备数据的时候直接 value = (value+qval)/2 同时考虑两者
             # self.data[fn]={"value":(value+qval)/2., "state":state, "mcts_prob": mcts_prob}
-            p = 0.75
-            v = qval*0.75 + value*(1-p)
+            p = 0.5
+            v = qval*p + value*(1-p)
             self.data[fn]={"value":v, "state":state, "mcts_prob": mcts_prob}
             sum_v+=value
             
