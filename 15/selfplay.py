e1a2866e76654b9cf72e4612f0961d1bf597bdbe
oneleaf <oneleaf@gmail.com>
2022-09-27 23:14:55 +0800

    .

--- 15/selfplay.py
+++ 15/selfplay.py
@@ -143,7 +143,7 @@ class Train():
 
         if random.random()>0.5 and os.path.exists(bestmodelfile):
             policy_value_net_best = PolicyValueNet(GAME_WIDTH, GAME_HEIGHT, GAME_ACTIONS_NUM, model_file=bestmodelfile)
-            player = MCTSPlayer((policy_value_net.policy_value_fn, policy_value_net_best.policy_value_fn), c_puct=self.c_puct, n_playout=self.n_playout)
+            player = MCTSPlayer(policy_value_net_best.policy_value_fn, c_puct=self.c_puct, n_playout=self.n_playout)
         else:
             player = MCTSPlayer(policy_value_net.policy_value_fn, c_puct=self.c_puct, n_playout=self.n_playout)
     
