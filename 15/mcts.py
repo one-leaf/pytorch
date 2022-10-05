86c8061a92cef5ea38b53bf8543e4e019e57da2e
oneleaf <oneleaf@gmail.com>
2022-10-05 15:30:24 +0800

    .

--- 15/mcts.py
+++ 15/mcts.py
@@ -203,9 +203,9 @@ class MCTS():
             else:
                 curr_pieceheight = game.pieceheight
                 next_pieceheight = other_game.pieceheight
-                if game.piececount - game.piececount_mark > 1:
-                    sv = (next_pieceheight - curr_pieceheight)/10
-                    return -sv
+                # if game.piececount - game.piececount_mark > 1:
+                #     sv = (next_pieceheight - curr_pieceheight)/10
+                #     return -sv
                 sv = (next_pieceheight-curr_pieceheight)/10
         v = sv + self.search(games)
         # v = self.search(games)
