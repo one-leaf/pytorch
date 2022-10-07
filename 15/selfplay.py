72de6a8c886deca7f9863769c7864bd6656f6b5e
oneleaf <oneleaf@gmail.com>
2022-10-07 09:08:03 +0800

    .

--- 15/selfplay.py
+++ 15/selfplay.py
@@ -208,7 +208,7 @@ class Train():
 
             # 如果训练次数超过了最大次数，则直接终止训练
             if i >= 10000: game_stop=True
-            if games[0].piececount>0 and games[1].piececount>0 and games[0].piececount==games[1].piececount:
+            if games[0].piececount>1 and games[1].piececount>1 and games[0].piececount==games[1].piececount:
                 if abs(games[0].pieceheight-games[1].pieceheight)>=2 or \
                     (games[0].score != games[1].score and games[0].pieceheight!=games[1].pieceheight): 
                     game_stop=True
