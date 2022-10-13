fd85477ca732641041f6db29f3af384384576a8d
oneleaf <oneleaf@gmail.com>
2022-10-13 22:16:27 +0800

    .

--- 15/selfplay.py
+++ 15/selfplay.py
@@ -114,7 +114,7 @@ class Train():
         print("TRAIN Self Play starting ...")
 
         # 游戏代理
-        agent = Agent(max_height=5, isRandomNextPiece=False)
+        agent = Agent(max_height=10, isRandomNextPiece=False)
 
         borads = []
 
@@ -212,6 +212,9 @@ class Train():
 
             # 如果训练次数超过了最大次数，则直接终止训练
             if i >= 10000: game_stop=True
+
+            if games[0].pieceheight>=5 or games[1].pieceheight>=5:
+                game_stop=True
             # if games[0].piececount>1 and games[1].piececount>1 and games[0].piececount==games[1].piececount:
             #     if (games[0].score != games[1].score and games[0].pieceheight!=games[1].pieceheight): 
             #         game_stop=True
