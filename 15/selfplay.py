68acb639456415114548ac531d1a4abbcfb8f0f1
oneleaf <oneleaf@gmail.com>
2022-10-18 11:53:26 +0800

    .

--- 15/selfplay.py
+++ 15/selfplay.py
@@ -366,7 +366,7 @@ class Train():
                 if piece_count!=data["steps"][j]["piece_count"]:
                     piece_count = data["steps"][j]["piece_count"]
                     score += data["steps"][j]["reward"]
-                    vlist.insert(0,(v+data["steps"][j]["qval"])/2)
+                    vlist.insert(0, round(data["steps"][j]["qval"],2))
                     slist.insert(0, score)
                     acclist.insert(0, data["steps"][j]["state_value"])
                 q = data["steps"][j]["qval"] 
