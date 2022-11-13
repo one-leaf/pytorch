8cc0ec9176ebcdb26a8fb7d9af1a1ca86b109173
oneleaf <oneleaf@gmail.com>
2022-11-13 15:38:23 +0800

    .

--- 16/model.py
+++ 16/model.py
@@ -35,7 +35,7 @@ class PolicyValueNet():
 
         self.l2_const = l2_const  
         # ViT-Ti : depth 12 width 192 heads 3
-        self.policy_value_net = VitNet(embed_dim=192, depth=12, num_heads=3, num_classes=4, num_quantiles=8, drop_ratio=0., drop_path_ratio=0., attn_drop_ratio=0.)
+        self.policy_value_net = VitNet(embed_dim=192, depth=12, num_heads=3, num_classes=4, num_quantiles=200, drop_ratio=0., drop_path_ratio=0., attn_drop_ratio=0.)
         # ViT-S : depth 12 width 386 heads 6
         # self.policy_value_net = VitNet(embed_dim=386, depth=12, num_heads=6, num_classes=4, num_quantiles=12)
         # ViT-B : depth 12 width 768 heads 12
