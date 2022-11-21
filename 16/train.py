6c8a6b0e4cf30753c5c6f447b9cb5589486c0121
oneleaf <oneleaf@gmail.com>
2022-11-21 19:51:31 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -140,13 +140,13 @@ class Dataset(torch.utils.data.Dataset):
         time.sleep(1)
         i = -1
         for i, fn in enumerate(movefiles):
-            if (i>=100 and i>len(movefiles)*0.1) or i>=self.max_keep_size//2: break       
-            if i>=self.max_keep_size//10: break       
             filename = os.path.basename(fn)
             savefile = os.path.join(self.data_dir, filename)
             if os.path.exists(savefile): os.remove(savefile)
             os.rename(fn, savefile)
             self.newsample.append(savefile)
+            if (i>=99 and i>len(movefiles)*0.1) or i>=self.max_keep_size//2: break       
+            if i>=self.max_keep_size//10: break       
         print("mv %s/%s files to train"%(i+1,len(movefiles)))
         if i==-1 :
             print("SLEEP 60s for watting data")
