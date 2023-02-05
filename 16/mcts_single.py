b754e071f8272eb70669d18ab6345d97af77586a
oneleaf <oneleaf@gmail.com>
2023-02-05 17:13:40 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -199,7 +199,8 @@ class MCTS():
 
         # 现实奖励
         if game.state == 1 and self.ext_reward and game.piececount - game.prev_piececount > 1:
-            v = game.prev_EmptyCount - game.emptyCount + game.prev_heightDiff - game.heightDiff + (game.prev_pieceheight - game.pieceheight)*0.1
+            v = game.prev_EmptyCount - game.emptyCount + game.prev_heightDiff - game.heightDiff
+            if v == 0: v = game.prev_pieceheight - game.pieceheight
             if v>0: v=v*0.1
         else:
             v = self.search(game)
