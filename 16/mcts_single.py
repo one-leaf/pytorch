a77cc46386de4c7fd0ad3635947a6e02c4af8e41
oneleaf <oneleaf@gmail.com>
2023-02-06 22:08:57 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -136,7 +136,7 @@ class MCTS():
         if self.depth>1000: return 0
 
         if game.terminal: 
-            self.Es[s] = -10
+            self.Es[s] = -1
 
         # 如果得分不等于0，标志探索结束
         if s in self.Es: return self.Es[s]
