9e1e25a44cdc14a3018118df9d9cc172fc755f86
oneleaf <oneleaf@gmail.com>
2023-02-12 11:23:26 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -144,7 +144,7 @@ class Train():
         # 锁定100
         n_playout = 100
         player = MCTSPlayer(policy_value_net.policy_value_fn, c_puct=self.c_puct, n_playout=n_playout)
-        # player.mcts.ext_reward = random.random()>0.2
+        player.mcts.ext_reward = True
 
         data = {"steps":[],"shapes":[],"last_state":0,"score":0,"piece_count":0}
         start_time = time.time()
