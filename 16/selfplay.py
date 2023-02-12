9d531794da5261b3a1dc0d9cd227c745050ba690
oneleaf <oneleaf@gmail.com>
2023-02-12 13:41:21 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -197,7 +197,7 @@ class Train():
                 # if agent.score>result["total"]["reward"]+20: game_stop=True
 
             # 如果训练次数超过了最大次数，则直接终止训练
-            if (agent.piececount>max_piececount and max_piececount>0) and agent.state == 1: game_stop=True
+            if not test_mode and (agent.piececount>max_piececount and max_piececount>0) and agent.state == 1: game_stop=True
 
             if agent.terminal or game_stop:
                 data["score"] = agent.score
