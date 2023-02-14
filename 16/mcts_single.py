9ada83ce334b2b2310e3e2b335ca5c80ada81a40
oneleaf <oneleaf@gmail.com>
2023-02-14 21:06:01 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -206,16 +206,17 @@ class MCTS():
         # 现实奖励
         if game.state == 1 and game.piececount - game.prev_piececount > 1:
             if self.ext_reward:
-                if game.heightStd<5:
-                    # 用空窗作为奖惩基准
-                    if game.prev_emptyCount > game.emptyCount:
-                        v = 1
-                    elif game.prev_emptyCount < game.emptyCount:
-                        v = -1
-                    else:
-                        v = 1 if game.prev_heightStd > game.heightStd or game.reward > 0 else -1
-                else:
-                    v = 1 if game.reward > 0 or game.prev_emptyCount > game.emptyCount else -1
+                # if game.heightStd<5:
+                #     # 用空窗作为奖惩基准
+                #     if game.prev_emptyCount > game.emptyCount:
+                #         v = 1
+                #     elif game.prev_emptyCount < game.emptyCount:
+                #         v = -1
+                #     else:
+                #         v = 1 if game.prev_heightStd > game.heightStd or game.reward > 0 else -1
+                # else:
+                
+                v = 1 if game.reward > 0 or game.prev_emptyCount > game.emptyCount else -1
 
                 # 鼓励标准差越小越好
                 # if game.prev_heightStd > game.heightStd:
