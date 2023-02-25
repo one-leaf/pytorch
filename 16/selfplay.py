8f85a628481e16ab3e0dc6b13f281274b2674319
oneleaf <oneleaf@gmail.com>
2023-02-25 10:57:52 +0800

    。

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -381,8 +381,6 @@ class Train():
         pieces_value = [0 for _ in range(agent.piececount)]
         # 局部的收益
         pieces_score = [0 for _ in range(agent.piececount)]
-        # 局部的损失
-        pieces_loss = [0 for _ in range(agent.piececount)]
         # 奖励的位置
         pieces_reward = [0 for _ in range(agent.piececount)]
         # 奖励的位置
@@ -402,10 +400,6 @@ class Train():
             if data["steps"][m]["reward"]>0:
                 pieces_reward[data["steps"][m]["piece_count"]] = 1
 
-        # 统计局部的损失
-        for m in range(1, agent.piececount):
-            pieces_loss[m] = min(data["steps"][pieces_steps[m]]["pre_piece_height"] - data["steps"][pieces_steps[m]]["piece_height"], 0)
-
         # 统计整体价值
         for m in range(agent.piececount):
             _r =  pieces_reward[m]
@@ -414,20 +408,22 @@ class Train():
                 pieces_value[n] += avg_r
 
         # 统计局部的收益
-        for m in range(agent.piececount):
-            p_r = 0
-            for n in range(m, agent.piececount):
-                _r =  pieces_reward[n]
-                avg_r = _r/(n-m+1)
-                p_r += avg_r
-            pieces_score[m] += p_r + pieces_loss[m]
+        for m in range(1, agent.piececount):
+            pieces_score[m] = data["steps"][pieces_steps[m]]["pre_piece_height"] - data["steps"][pieces_steps[m]]["piece_height"]
+
+        # 统计局部的收益
+        # for m in range(agent.piececount):
+        #     p_r = 0
+        #     for n in range(m, agent.piececount):
+        #         _r =  pieces_reward[n]
+        #         avg_r = _r/(n-m+1)
+        #         p_r += avg_r
+        #     pieces_score[m] += p_r + pieces_loss[m]
 
         print()
         print(i, pieces_reward)
         print()
         print(i, pieces_value)
-        print()
-        print(i, pieces_loss)
         print()
         print(i, pieces_score)
         print()
