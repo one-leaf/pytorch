8dca85355c26ff9603fb621aaee7889a68202478
oneleaf <oneleaf@gmail.com>
2023-03-04 13:04:06 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -169,8 +169,8 @@ class Train():
                     break
             agent.print()
 
-            # 判断是否需要重新玩,如果当前奖励小于平均奖励的1/2，放到运行池训练
-            if agent.score>0 and agent.score < result["total"]["avg_score"]/2:
+            # 判断是否需要重新玩,如果当前奖励小于平均奖励的1/3，放到运行池训练
+            if agent.piececount < result["total"]["avg_piececount"]/3:
                 his_pieces = agent.tetromino.piecehis
                 filename = "{}-{}.pkl".format(agent.score, int(round(time.time() * 1000000)))
                 savefile = os.path.join(self.waitplaydir, filename)
