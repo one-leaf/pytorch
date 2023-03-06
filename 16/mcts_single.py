0c58cb2a1068bb61ae886ec5143d65bd94088e4b
oneleaf <oneleaf@gmail.com>
2023-03-06 10:11:56 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -272,7 +272,7 @@ class MCTSPlayer(object):
             max_ps_idx = np.argmax(act_ps)
 
             # 尝试其他的走法
-            if 1 or random.random()<(game.piececount - avg_piececount)/(avg_piececount+1):
+            if game.piececount<2 or random.random()<(game.piececount - avg_piececount)/(avg_piececount+1):
                 p = 0.75
                 # a=1的时候，dir机会均等，>1 强调均值， <1 强调两端
                 # 国际象棋 0.3 将棋 0.15 围棋 0.03
