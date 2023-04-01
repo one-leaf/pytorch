c3840e91a37b88a265d4b5bed68c3ea14ebc1aca
oneleaf <oneleaf@gmail.com>
2023-04-01 13:36:56 +0800

    .

--- torch.sh
+++ torch.sh
@@ -1,3 +1,5 @@
+#sudo mount -t ramfs none 16/data
+
 docker run --name torch --gpus device=1 -it -p 5901:5901 -p 6901:6901 -v $PWD:/mnt -w /mnt -v /etc/localtime:/etc/localtime:ro pytorch/pytorch:1.5.1-cuda10.1-cudnn7-runtime bash
 
 docker run --gpus device=1 -it --rm -v $PWD:/mnt -w /mnt -v /etc/localtime:/etc/localtime:ro pytorch-opencv:v12 bash
