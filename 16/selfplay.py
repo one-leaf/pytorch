d976dc23ce054f806db9a8066127fe03816121ee
oneleaf <oneleaf@gmail.com>
2023-05-15 18:41:00 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -432,10 +432,10 @@ class Train():
 
         # 游戏的得分算法1，每一步都减1，如果碰到奖励，不重置步骤，最终/除以全局的奖励
         # 最大失分为100
-        score_mask = (sum(pieces_reward)+1)*agent.piececount/100
+        score_mask = (sum(pieces_reward)+1)*(agent.piececount+1)/100
         for m in range(step_count):
             p =  data["steps"][m]["piece_count"]
-            data["steps"][m]["value"]= -p/score_mask
+            data["steps"][m]["value"]= -(p+1)/score_mask
             # if pieces_reward[p]>0 and m==pieces_steps[p] : 
             #     score_mask=score_mask-1
         pieces_value = [data["steps"][pieces_steps[p]]["value"] for p in range(piececount)]
