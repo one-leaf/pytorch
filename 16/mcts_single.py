9984948da36fd3f0566880e325e313ab802f2e97
oneleaf <oneleaf@gmail.com>
2023-07-08 10:26:40 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -276,8 +276,8 @@ class MCTSPlayer(object):
             # if max_qs_idx ==  max_ps_idx:
             #     idx = max_qs_idx                
             # elif (random.random() > 2*game.piececount/(avg_piececount+1))  or random.random()<(game.piececount - avg_piececount)/(avg_piececount+1):
-            if game.score==0 :
-                p = 0.85
+            if True or game.score==0 :
+                p = 0.9
                 # a=1的时候，dir机会均等，>1 强调均值， <1 强调两端
                 # 国际象棋 0.3 将棋 0.15 围棋 0.03
                 # 取值一般倾向于 a = 10/n 所以俄罗斯方块取 2
