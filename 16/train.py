1211870bfedd663ee83663b913c916500001e5ce
oneleaf <oneleaf@gmail.com>
2023-07-26 09:33:54 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -277,7 +277,7 @@ class Train():
             for i, data in enumerate(training_loader):  # 计划训练批次
                 # 使用对抗数据重新训练策略价值网络模型
                 _, v_loss, p_loss = self.policy_update(data, self.epochs)
-                if i%10 == 0:
+                if i%100 == 0:
                     print(i,"v_loss:",v_loss,"p_loss",p_loss)
                 if math.isnan(v_loss): 
                     print("v_loss is nan!")
