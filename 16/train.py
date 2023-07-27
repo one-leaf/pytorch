c9d50ecca44e7987f83c524d31cf78051bb9cdd8
oneleaf <oneleaf@gmail.com>
2023-07-27 19:30:33 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -203,9 +203,9 @@ class Train():
         self.learn_rate = 1e-5
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
-        self.n_playout = 256  # 每个动作的模拟战记录个数
+        self.n_playout = 64  # 每个动作的模拟战记录个数
         self.play_batch_size = 1 # 每次自学习次数
-        self.buffer_size = 320000  # cache对次数
+        self.buffer_size = 200000  # cache对次数
         self.epochs = 1  # 每次更新策略价值网络的训练步骤数, 推荐是5
         self.kl_targ = 0.1  # 策略价值网络KL值目标        
         self.c_puct = 2  # MCTS child权重， 用来调节MCTS中 探索/乐观 的程度 默认 5
