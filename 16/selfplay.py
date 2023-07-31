edd7524523e2d9db4a9df825abf0dad26020788d
oneleaf <oneleaf@gmail.com>
2023-07-31 10:57:39 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -38,7 +38,7 @@ class Train():
         # aplhatensor 是 5
         # MCTS child权重， 用来调节MCTS搜索深度，越大搜索越深，越相信概率，越小越相信Q 的程度 默认 5
         # 由于value完全用结果胜负来拟合，所以value不稳，只能靠概率p拟合，最后带动value来拟合
-        self.c_puct = 1  
+        self.c_puct = 5  
 
         self.max_step_count = 10000 
 
