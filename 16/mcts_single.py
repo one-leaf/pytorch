77499a4f6de5a7b6e459c29c5c4e07cff4d94b66
oneleaf <oneleaf@gmail.com>
2023-08-04 21:37:12 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -71,6 +71,7 @@ class MCTS():
             
             if n >= self._n_playout/2-1 and not game.is_status_optimal(): break
             if n >= self._n_playout-1 : break
+            if n >= self._n_playout-game.score : break
             # if game_.piececount - game.prev_piececount>1 and n>self._n_playout/2: break
             # if self.depth < 200 and n < self._n_playout*10: continue 
 
