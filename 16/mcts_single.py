b88490e64e1c2703b1605fd863e0928e9ff705d4
oneleaf <oneleaf@gmail.com>
2023-08-26 12:12:47 +0800

    .

--- 16/mcts_single.py
+++ 16/mcts_single.py
@@ -166,7 +166,7 @@ class MCTS():
             # 获得当前局面的概率 和 局面的打分, 这个已经过滤掉了不可用走法
             act_probs, v = self._policy(game)
 
-            if game.exreward:
+            if game.exreward and game.state==1:
                 # if game.score > game.prev_score:
                 #     v += (game.score-game.prev_score)
                 if game.prev_emptyCount == game.emptyCount:
