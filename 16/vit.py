0642e79f803a209425173c51c1694d2c0e953a87
oneleaf <oneleaf@gmail.com>
2023-08-26 09:07:30 +0800

    .

--- 16/vit.py
+++ 16/vit.py
@@ -357,7 +357,7 @@ class VitNet(nn.Module):
 
         self.val_fc = nn.Linear(embed_dim, embed_dim)   # [B, 768] => [B, 768]
         self.val_fc_act = nn.GELU()
-        self.val_dist = nn.Linear(embed_dim, num_quantiles)   # [B, 768] => [B, 1]
+        self.val_dist = nn.Linear(embed_dim, num_quantiles)   # [B, 768] => [B, num_quantiles]
         self.val_dist_act = nn.Tanh()
 
         # 参数初始化, 这里需要pytorch 1.6以上版本
