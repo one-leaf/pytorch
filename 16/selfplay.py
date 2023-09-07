86b0411c77639dea5ad4c5f56a666e20af2cd1a3
oneleaf <oneleaf@gmail.com>
2023-09-07 15:42:35 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -27,6 +27,8 @@ class Train():
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
         self.n_playout = 256  # 每个动作的模拟战记录个数，影响后续 512/2 = 256；256/16 = 16个方块 的走法
         # 64/128/256/512 都不行
+        # step -> score
+        # 128  --> 0.7
         # self.n_playout = 128  # 每个动作的模拟战记录个数，影响后续 128/2 = 66；64/16 = 4个方块 的走法
         self.play_size = 10 # 每次测试次数
         self.buffer_size = 1000000  # cache对次数
