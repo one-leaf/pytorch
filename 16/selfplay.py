899e40ffb19eb205f277094464df9901ae8cb9e9
oneleaf <oneleaf@gmail.com>
2023-09-13 12:54:29 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -343,7 +343,7 @@ class Train():
                 ns = np.average(ns)
 
                 if agent.exreward:
-                    result["exrewardRate"][exrewardRateKey] = round(agent.score*0.01 + float(result["exrewardRate"][exrewardRateKey])*0.99, 2)
+                    result["exrewardRate"][exrewardRateKey] = round(agent.score*0.1 + float(result["exrewardRate"][exrewardRateKey])*0.9, 2)
 
                 if result["total"]["pacc"]==0:
                     result["total"]["pacc"] = pacc
