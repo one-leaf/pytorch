5b76242366de412d03fecd2d157ca1a2717ae6da
oneleaf <oneleaf@gmail.com>
2023-11-19 17:51:29 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -269,7 +269,7 @@ class MCTS():
             # r = np.tanh(r)
         
         # 更新 Q 值 和 访问次数      
-        v = r + 0.99*v 
+        v = r + v 
         updateQN(s, a, v, self.Ns, self.Qsa, self.Nsa, state.actions_num)
 
         # print(v, self.Qsa[s][a], v-self.Qsa[s][a])
