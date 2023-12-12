d7f10041735b139d05a7b6a17ba2a00e5129668d
oneleaf <oneleaf@gmail.com>
2023-12-12 09:43:43 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -226,7 +226,7 @@ class MCTS():
             print(time.strftime('%m-%d %H:%M:%S',time.localtime(time.time())), game.steps, game.fallpiece["shape"], \
                   "ns:", str(ns).rjust(4), "/", str(self.simulation_count).ljust(4), "depth:", str(self.max_depth).ljust(3), \
                 #   "\tQ:", round(v,2), "-->",round(qs[max_p],2), '/', round(qs[max_q],2), \
-                  "\tV:", round(v  ,2), \
+                  "\tV:", round(v,2), "-->", round(max_q,2) \
                   "\tP:", round(ps[max_p],2), "-->", round(probs[max_p],2),'/', round(probs[max_q],2), \
                   "\tQs:", qs, "var", np.var(qs[nz_idx]))
         # 动作数，概率，每个动作的Q，原始概率，当前局面的v，当前局面的总探索次数
