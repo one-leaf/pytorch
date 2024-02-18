0f61b562c00d06478e6bfa5f28eb886e2e8d35f8
oneleaf <oneleaf@gmail.com>
2024-02-18 09:29:14 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -353,7 +353,7 @@ class MCTSPlayer(object):
             #     idx = np.random.choice(range(ACTONS_LEN), p=act_probs)    
 
             # 如果返回都是错，不用管NS，直接用QS随机
-            if game.steps % 2 == 0 or (act_ps[max_ps_idx]>0.98 and act_probs[max_ps_idx]>0.98):
+            if game.steps % 2 == 0 or (max_qs_idx==max_probs_idx):
                 idx = max_ps_idx
             else:
                 if len(nz_idx[0])==4 and var_qs<0.001:
