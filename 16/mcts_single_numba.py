c50db9b4b9ef7fcabb4cc30f77b472b4748c1585
oneleaf <oneleaf@gmail.com>
2024-03-21 23:06:30 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -215,7 +215,7 @@ class MCTS():
             if depth > self.max_depth: self.max_depth = depth
             if self.simulation_count>=10 and depth>self.limit_depth and state_.game.state==1 and self.Ns[s]>=100: break
             if state.game.piececount==0:
-                if self.simulation_count >= self._n_playout//4 and state_.game.state==1 : break 
+                if self.simulation_count >= self._n_playout//8 and state_.game.state==1 : break 
             else:
                 if self.simulation_count >= self._n_playout and state_.game.state==1 : break 
 
