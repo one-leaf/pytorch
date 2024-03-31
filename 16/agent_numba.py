4980334f36295b3c77faa017d77dd191928b97b6
oneleaf <oneleaf@gmail.com>
2024-03-31 17:28:41 +0800

    .

--- 16/agent_numba.py
+++ 16/agent_numba.py
@@ -743,7 +743,7 @@ class Agent():
             # 这里强制20个方块内必需合成一行个 和 强制第一个方块之后不能有空
             if ( not self.validposition(self.board, self.fallpiece, ay=0) or \
                                (self.limitstep and self.piececount-self.last_reward>=20) or \
-                               (self.limitstep and putEmptyBlock and reward==0 and self.piececount>1) ):
+                               (self.limitstep and putEmptyBlock and reward==0 and self.emptyCount>1) ):
                 self.terminal = True 
                 self.state = 1
         else:
