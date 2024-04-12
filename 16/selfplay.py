f02e1d7125d56167cf759b094c00648bd87930f2
oneleaf <oneleaf@gmail.com>
2024-04-12 10:38:22 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -222,9 +222,9 @@ class Train():
             _step["shape"] = agent.fallpiece["shape"]
             _step["pre_piece_height"] = agent.pieceheight
 
-            if agent.piecesteps==0 and random.random()>0.5: 
-                need_max_ps=not need_max_ps
-                print("switch need_max_ps to:", need_max_ps)
+            # if agent.piecesteps==0 and random.random()>0.5: 
+            #     need_max_ps=not need_max_ps
+            #     print("switch need_max_ps to:", need_max_ps)
                 
             action, qval, move_probs, state_value, max_qval, acc_ps, depth, ns = player.get_action(agent, temp=1, need_max_ps=need_max_ps) 
 
