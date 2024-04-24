da0808333125f4e3a5095c5ae6f59519aa215674
oneleaf <oneleaf@gmail.com>
2024-04-24 14:37:53 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -497,7 +497,7 @@ class Train():
                         result["total"]["exrewardRate"]+=(result["qval"][-2]-result["qval"][-1])*0.1
                     
                     if result["total"]["exrewardRate"]<0:
-                        result["total"]["exrewardRate"]=1e-3
+                        result["total"]["exrewardRate"]=4e-3
                         
             result["lastupdate"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             self.save_status_file(result, game_json) 
