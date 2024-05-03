d4099d068cf1f713417aa868000df967958a4e1c
oneleaf <oneleaf@gmail.com>
2024-05-04 00:28:19 +0800

    .

--- 03/09.py
+++ 03/09.py
@@ -35,11 +35,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 fake_loss = model(x_train).sum()
 fake_loss.backward()
+
+# 参数的长度132353，每一层都需要计算梯度，所以需要除以sqrt(hidden_dim)，确保梯度大小一致
 param_dim = torch.cat(
         [w.grad.detach().flatten() / np.sqrt(hidden_dim) for w in model.parameters() if w.requires_grad]
     ).size()[0]
 
-# func
+# 计算梯度
 def compute_grad(model, optimizer, x):
     assert x.shape[0] == 1
     y = model(x)
@@ -49,14 +51,15 @@ def compute_grad(model, optimizer, x):
         [w.grad.detach().flatten() / np.sqrt(hidden_dim) for w in model.parameters() if w.requires_grad]
     )
 
+# 计算雅克比矩阵，根据样本计算梯度/偏导数
 def compute_jac(model, optimizer, xs):
     jacobian = torch.zeros(xs.shape[0], param_dim, device=xs.device)
     for i, x in enumerate(xs):
         x = x.view(1, -1)
         jacobian[i] = compute_grad(model, optimizer, x)
-
     return jacobian
 
+# 求雅可比矩阵的逆
 def compute_kernel_inv(jac, eps=1e-5):
     return torch.linalg.inv(torch.matmul(jac,jac.T)+eps*torch.diag(torch.ones(n_samples)))
 
@@ -64,14 +67,18 @@ def predict(xs_test, x, y, jac, G_inv, model, optimizer):
     ys_test = torch.zeros(xs_test.shape[0], device=xs_test.device)
     for i, x_test in enumerate(xs_test):
         x_test = x_test.view(1,-1)
+        # 计算单个验证数据的梯度
         g = compute_grad(model, optimizer, x_test).view(1,-1) # (1, param-dim,)
+        # 与雅可比矩阵的转置相乘得到协方差矩阵
         K = torch.matmul(g, jac.T) # (1, n_samples)
-        ys_test[i] = torch.matmul(torch.matmul(K, G_inv), y) + model(x_test).detach() - torch.matmul(torch.matmul(K, G_inv), model(x_train).detach())
+        # y_test = 模型训练集x_test的结果 + x_test和训练集y的积 - x_test和训练集x的积
+        ys_test[i] = model(x_test).detach() + torch.matmul(torch.matmul(K, G_inv), y) - torch.matmul(torch.matmul(K, G_inv), model(x_train).detach())
     return ys_test
 
 # exec
 jac = compute_jac(model, optimizer, xs=x_train)
 G_inv = compute_kernel_inv(jac, eps=eps)
+
 y_test = predict(x_fun, x_train, y_train, jac, G_inv, model, optimizer)
 plt.plot(x_fun, y_test.numpy())
 plt.plot(x_fun, y_fun)
