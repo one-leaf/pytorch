6e25775c04c217f9069f197e3828c630068d240d
oneleaf <oneleaf@gmail.com>
2024-06-17 17:31:54 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -249,7 +249,7 @@ class MCTS():
                   "\tV:", round(v,2), "-->", round(qs[max_q_idx],2), \
                   "\t%s %s:"%(game.position_to_action_name(max_q_idx),game.position_to_action_name(max_p)), \
                   round(ps[max_p],2), "-->", round(probs[max_p],2), \
-                  "\tQs:", qs, "var", np.var(self.Nsa[s]))
+                  "\tQs:", qs, "var", np.var(probs))
             # 如果这一局已经超过了20分钟
             # if run_time>20*60 and self.limit_depth!=20:
             #     print("limit max depth to 20")
