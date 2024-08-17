ed7d5dd1d512ad0637e0ee37ac966943047817db
oneleaf <oneleaf@gmail.com>
2024-08-17 17:45:17 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -191,12 +191,12 @@ class Train():
         if his_pieces!=None:
             print("min_removedlines:", min_removedlines, "pieces_count:", len(his_pieces))
             print("his_pieces:", his_pieces)
-            agent = Agent(isRandomNextPiece=False, nextPiecesList=his_pieces)
+            agent = Agent(isRandomNextPiece=True, nextPiecesList=his_pieces)
             agent.is_replay = True
             agent.limitstep = True
         else:
             # 新局按Q值走，探索
-            agent = Agent(isRandomNextPiece=False, )
+            agent = Agent(isRandomNextPiece=True, )
             agent.is_replay = False
             agent.limitstep = False
 
