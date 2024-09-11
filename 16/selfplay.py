2934044bd19f7899a3a6973f3763115ed8f6f567
oneleaf <oneleaf@gmail.com>
2024-09-11 10:18:21 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -268,7 +268,7 @@ class Train():
             # 如果游戏结束或玩了超过1小时或10个方块都没有消除一行
             paytime = time.time()-start_time
                 # (agent.removedlines > result["total"]["avg_score"]+1)  or \
-            if agent.terminal or (agent.state==1 and paytime>60*10) :#or agent.piececount-agent.last_reward>=10:
+            if agent.terminal or (agent.state==1 and paytime>60*10) or reward>0:#or agent.piececount-agent.last_reward>=10:
                 data["score"] = agent.score
                 data["piece_count"] = agent.piececount
                 data["piece_height"] = agent.pieceheight
