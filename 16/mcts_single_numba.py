6626bd77aa4a2d9ed080fabaf08298cae8538e0a
oneleaf <oneleaf@gmail.com>
2024-10-13 14:45:16 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -290,9 +290,9 @@ class MCTS():
             expandPN(s, availables, act_probs, self.Ps, self.Ns, self.Nsa, self.Qsa, state.actions_num)             
             # v = v + r*10 
             # v *= 0.5 # 测试稳定网络用 v * 0.5 + reward ==> v ; v ==> 2 * reward
-            v = float(v)
+            v = float(v)+r
             self.Vs[s] = v
-            v = 0.5 * (v + r)
+            v *= 0.5 
             return v
 
         # 当前最佳概率和最佳动作
