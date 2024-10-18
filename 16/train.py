0aa6289562dd8bd288c4c09bff5b30496af26f32
oneleaf <oneleaf@gmail.com>
2024-10-18 21:21:38 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -108,7 +108,7 @@ class Dataset(torch.utils.data.Dataset):
                     state, mcts_prob, value, reward = pickle.load(f)
                     # if len(mcts_prob)==4:
                     #     mcts_prob = np.concatenate((mcts_prob, np.zeros(1)), axis=0)
-                    if reward<0: reward=-1
+                    # if reward<0: reward=-1
             except:
                 print("filename {} error can't load".format(fn))
                 if os.path.exists(fn): os.remove(fn)
