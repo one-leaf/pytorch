df78bb085e92f6cb7bd9dba59e2f52f96e9bd335
oneleaf <oneleaf@gmail.com>
2024-10-19 23:27:17 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -437,7 +437,7 @@ class MCTSPlayer(object):
                 # dirichlet_probs[_id_list] = dirichlet
                 # idx = np.random.choice(range(ACTONS_LEN), p=p*fix_act_probs + (1.0-p)*dirichlet_probs)
                 
-                dirichlet = np.random.dirichlet(2 * np.ones(len(nz_idx[0])))
+                dirichlet = np.random.dirichlet(2 * np.ones(len(nz_idx)))
                 dirichlet_probs = np.zeros_like(act_probs, dtype=np.float64)
                 dirichlet_probs[nz_idx] = dirichlet
                 idx = np.random.choice(range(ACTONS_LEN), p=p*act_probs + (1.0-p)*dirichlet_probs)
