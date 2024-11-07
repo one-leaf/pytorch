2cad834179aafd02ac114757fde1b0e71f6157f0
oneleaf <oneleaf@gmail.com>
2024-11-07 17:51:06 +0800

    。

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -260,7 +260,7 @@ class MCTS():
                   "ns:", str(nsv).rjust(4), "/", str(self.simulation_count).ljust(4), "depth:", str(self.max_depth).ljust(3), \
                 #   "\tQ:", round(v,2), "-->",round(qs[max_p],2), '/', round(qs[max_q],2), \
                   "die:", die_count, "v:", round(v,2), "r:", round(r,2), \
-                  "\t%s %s:"%(game.position_to_action_name(max_q_idx),game.position_to_action_name(max_p)), \
+                  "\t%s:"%game.position_to_action_name(max_p), \
                   round(ps[max_p],2), "-->", round(probs[max_p],2), \
                   "\tQs:", qs, "\tNs:", ns, "\tPs:", ps)
             # 如果这一局已经超过了20分钟
