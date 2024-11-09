a1c0d9c6482eaee0579891b1b001f19151841017
oneleaf <oneleaf@gmail.com>
2024-11-09 11:37:59 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -262,7 +262,7 @@ class MCTS():
             print(timedelta(seconds=run_time), game.steps, game.fallpiece["shape"], \
                   "ns:", str(nsv).rjust(4), "/", str(self.simulation_count).ljust(4), "depth:", str(self.max_depth).ljust(3), \
                 #   "\tQ:", round(v,2), "-->",round(qs[max_p],2), '/', round(qs[max_q],2), \
-                  "die:", die_count, "v:", round(v,2), "r:", round(r,2), \
+                  "end:", die_count, "v:", round(v,2), "r:", round(r,2), \
                   game.position_to_action_name(max_p), round(ps[max_p],2), "-->", round(probs[max_p],2), "\t", mask, \
                   "Qs:", qs, "\tNs:", ns, "\tPs:", ps)
             # 如果这一局已经超过了20分钟
