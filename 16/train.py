f2362ef427d5c623e26c0a04913b81a2dff2b6f7
oneleaf <oneleaf@gmail.com>
2024-12-12 19:34:44 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -381,10 +381,6 @@ class Train():
             # kl = np.mean(np.sum(begin_act_probs * (np.log(begin_act_probs) - np.log(end_act_probs)), axis=1))
             kl = np.mean(np.sum(begin_act_probs * (np.log(begin_act_probs/end_act_probs)), axis=1))
             if np.isnan(kl):
-                print(begin_act_probs_e)
-                print(np.sum(begin_act_probs_e, axis=1, keepdims=True))
-                print("kl error, is nan")
-                print("act_probs, kl:",kl)
                 kl = 0
             if kl == 0:
                 pass                
