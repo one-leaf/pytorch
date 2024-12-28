ddc895c0569987943acb3fc556890b3da0c0c7d3
oneleaf <oneleaf@gmail.com>
2024-12-28 23:58:39 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -126,7 +126,7 @@ class Train():
         for i in range(len(states)):
             state, mcts_prob, value, score=states[i], mcts_probs[i], values[i], scores[i]
             extend_data.append((state, mcts_prob, value, score))
-            if mcts_prob[0]<0.1: # 如果旋转的概率的不大，就做翻转
+            if mcts_prob[0]<0.2 and np.max(mcts_prob)>0.8: # 如果旋转的概率的不大，就做翻转
                 equi_state = np.array([np.fliplr(s) for s in state])
                 equi_mcts_prob = mcts_prob[[0,2,1,3,4]]
                 extend_data.append((equi_state, equi_mcts_prob, value, score))
