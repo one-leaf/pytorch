0ba6bafe22ed0e686a09d49e719c1aa5a675e922
oneleaf <oneleaf@gmail.com>
2024-12-29 18:57:38 +0800

    .

--- 16/vit.py
+++ 16/vit.py
@@ -483,19 +483,19 @@ class VitNet(nn.Module):
         self.norm = norm_layer(embed_dim)
 
         self.act_fc = nn.Linear(embed_dim, embed_dim)  # [B, 768] => [B, 768]
-        self.norm_act = norm_layer(embed_dim)
+        # self.norm_act = norm_layer(embed_dim)
         self.act_fc_act = nn.LeakyReLU() # nn.GELU()
         self.act_dist = nn.Linear(embed_dim, num_classes)  # [B, 768] => [B, 5]
         # self.act_dist_act = nn.Softmax(dim=1)
 
         self.val_fc = nn.Linear(embed_dim, embed_dim)   # [B, 768] => [B, 768]
-        self.norm_val = norm_layer(embed_dim)
+        # self.norm_val = norm_layer(embed_dim)
         self.val_fc_act = nn.LeakyReLU() # nn.GELU()
         self.val_dist = nn.Linear(embed_dim, num_quantiles)   # [B, 768] => [B, num_quantiles]
         self.val_dist_act = nn.Tanh()
 
         self.q_fc = nn.Linear(embed_dim, embed_dim)   # [B, 768] => [B, 768]
-        self.norm_q = norm_layer(embed_dim)        
+        # self.norm_q = norm_layer(embed_dim)        
         self.q_fc_act = nn.LeakyReLU() # nn.GELU()
         self.q_dist = nn.Linear(embed_dim, 1)   # [B, 768] => [B, 1]
         self.q_dist_act = nn.Tanh()
@@ -528,7 +528,7 @@ class VitNet(nn.Module):
         x = self.blocks(x)                    # [B, p+3, 768]
 
         # 归一化
-        x = self.norm(x)                        # [B, p+3, 768]
+        # x = self.norm(x)                        # [B, p+3, 768]
 
         # 将模型动作和价值网络相对隔离
         act = self.act_fc(x[:, 0])
