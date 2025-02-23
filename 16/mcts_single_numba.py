8b25b60461e42bc1e050212a46f15fec97a3b63f
oneleaf <oneleaf@gmail.com>
2025-02-23 19:50:56 +0800

    。

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -361,7 +361,7 @@ class MCTS():
             #     v = r
             # else:
             #     v = self.search(state)
-            v = r + self.search(state)
+            v = self.search(state) + r
         #     # 不鼓励主动消行，以局面为主
             # if state.markEmptyCount>state.game.emptyCount:
             #     v += (state.markEmptyCount-state.game.emptyCount)**2 * state.game.exrewardRate
