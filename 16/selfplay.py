5564cba2824e724fe68ef7c38a29dc69a0fc284a
oneleaf <oneleaf@gmail.com>
2025-04-29 11:54:06 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -283,7 +283,7 @@ class Train():
 
         # 检查有没有需要重复运行的
         listFiles = [f for f in os.listdir(self.waitplaydir) if f.endswith(".pkl")]
-        if listFiles: # and random.random()>0.75:
+        if listFiles and random.random()>0.50:
             # earliest_file = min(listFiles, key=lambda f: os.path.getctime(os.path.join(self.waitplaydir, f)))
             earliest_files = sorted(listFiles)
             
