78a7356a003c2d01c9ebaaa2f8861e64a3b6b2a1
oneleaf <oneleaf@gmail.com>
2025-06-02 14:35:43 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -137,9 +137,9 @@ class Dataset(torch.utils.data.Dataset):
         std_values = np.std(values_items)
 
         for fn in values:
-            value = (values[fn]-avg_values)/std_values
-            if value>1: value=1-1e-6
-            if value<-1: value=-1+1e-6
+            # value = (values[fn]-avg_values)/std_values
+            if value>=1: value=1-1e-6
+            if value<=-1: value=-1+1e-6
             values[fn] = value
 
         print("value min/avg/max/std:",[min_values, avg_values, max_values, std_values])        
