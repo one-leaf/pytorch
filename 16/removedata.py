5b809ba74db2d584bdc89733a631f204483f1ca9
oneleaf <oneleaf@gmail.com>
2025-06-11 16:42:57 +0800

    。

---
+++ 16/removedata.py
@@ -0,0 +1,14 @@
+# Remove data from the data folder
+import os
+from model import PolicyValueNet, data_dir, data_wait_dir, model_file
+
+def remove_data():
+    for root, dirs, files in os.walk(data_dir, topdown=False):
+        for name in files:
+            if name.endswith(".pkl"):
+                os.remove(os.path.join(root, name))
+                
+if __name__ == "__main__":
+    remove_data()
+    print("All .pkl files removed from the data folder.")
+    
