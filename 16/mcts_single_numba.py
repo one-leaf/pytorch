e256193c8cc350c785a16bf3db86623f78a463b8
oneleaf <oneleaf@gmail.com>
2025-06-12 21:06:08 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -396,7 +396,7 @@ class MCTSPlayer(object):
                 # a = 2       
                 # p=0.999**(time.time()-game.start_time)/60     # 每1秒减少0.1的概率
                 # if p<0.1: p=0.1
-                p = 0.999**game.removedlines
+                p = 0.998**game.removedlines
                 dirichlet = np.random.dirichlet(2 * np.ones(len(nz_idx)))
                 dirichlet_probs = np.zeros_like(act_probs, dtype=np.float64)
                 dirichlet_probs[nz_idx] = dirichlet
