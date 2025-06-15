180dd1041b52dccb088072ba593d35f619f8f2cb
oneleaf <oneleaf@gmail.com>
2025-06-15 19:36:12 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -390,7 +390,7 @@ class MCTSPlayer(object):
                       
             p = 0     
             # if idx == -1 or min(act_qs)<0:    
-            if idx == -1 or min(act_qs)>=0 or (max(act_qs)>0 and game.removedlines > self.min_score):
+            if idx == -1 or min(act_qs)>=0 :
                 # a=1的时候，act 机会均等，>1 强调均值， <1 强调两端
                 # 国际象棋 0.3 将棋 0.15 围棋 0.03
                 # 取值一般倾向于 a = 10/n 所以俄罗斯方块取 2
