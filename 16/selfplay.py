256b22fc33c50b66eb2a9b3e6b176ffb6e08f3d8
oneleaf <oneleaf@gmail.com>
2025-07-22 09:49:44 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -29,7 +29,7 @@ class Train():
         self.learn_rate = 1e-8
         self.lr_multiplier = 1.0  # 基于KL的自适应学习率
         self.temp = 1  # MCTS的概率参数，越大越不肯定，训练时1，预测时1e-3
-        self.n_playout = 32  # 每个动作的模拟战记录个数，影响后续 512/2 = 256；256/16 = 16个方块 的走法
+        self.n_playout = 64  # 每个动作的模拟战记录个数，影响后续 512/2 = 256；256/16 = 16个方块 的走法
         # self.min_n_playout = 64   # 最小的模拟战记录个数
         # self.max_n_playout = 256  # 最大1的模拟战记录个数
         # 64/128/256/512 都不行
@@ -48,7 +48,7 @@ class Train():
         # aplhatensor 是 5
         # MCTS child权重， 用来调节MCTS搜索深度，越大搜索越深，越相信概率，越小越相信Q 的程度 默认 5
         # 由于value完全用结果胜负来拟合，所以value不稳，只能靠概率p拟合，最后带动value来拟合
-        self.c_puct = 0.1  
+        self.c_puct = 1  
         self.sample_count = 512  # 每次采样的样本数
         self.max_step_count = 10000 
         self.limit_steptime = 1  # 限制每一步的平均花费时间，单位秒，默认1秒
