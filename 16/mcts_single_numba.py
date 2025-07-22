006af13bd4f536001579327bc2ee154f8545e39e
oneleaf <oneleaf@gmail.com>
2025-07-22 11:04:51 +0800

    。

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -284,7 +284,7 @@ class MCTS():
                 
         v = self.search(state) 
         # v += _removedlines*0.01   # 每消除一行奖励0.01，奖励越多，分数越高
-        v -= _emptyCount*0.01       # 每多一个空洞扣0.01
+        # v -= _emptyCount*0.01       # 每多一个空洞扣0.01
         
         # 外部奖励，放的方块越多越好
         # if state.game.state==1:
