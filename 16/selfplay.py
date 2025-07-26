cf08b2402c7cd245368d4f1fbd53aa836bfd9921
oneleaf <oneleaf@gmail.com>
2025-07-26 18:36:22 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -590,7 +590,7 @@ class Train():
             
             for k in range(len_steps):
                 step = play_data[i]["data"]["steps"][k]
-                step["qval"] = (np.mean(data[k:k+8]) - step["qval"])
+                step["qval"] = (np.mean(data[k:k+4]) - step["qval"])
                 # if k==len_steps-1:
                 #     step["qval"] = -1
                 # else:
