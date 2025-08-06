3113a151de0204046944789cccddb712752475a6
oneleaf <oneleaf@gmail.com>
2025-08-06 12:00:17 +0800

    .

--- 16/mcts_single_numba.py
+++ 16/mcts_single_numba.py
@@ -396,13 +396,13 @@ class MCTSPlayer(object):
             # if game.removedlines > self.min_score: 
             #     idx = max_ps_idx     
             # if game.steps < self.limit_count//2:
-            if self.player==0:
+            if self.player==0 and random.random()>1/(game.piececount+1):
                 if len(game.piecehis) < 2*game.next_Pieces_list_len:
                     if random.random() < 0.99:
                         idx = max_ns_idx
                     else:
                         idx = max_qs_idx
-            elif self.player==1:
+            elif self.player==1 and random.random()>1/(game.piececount+1):
                 if len(game.piecehis) < 2*game.next_Pieces_list_len:
                     if random.random() < 0.99:
                         idx = max_ps_idx    
