d8361fcd7f5b6ab04ea64b8a6be85146d4c224cc
oneleaf <oneleaf@gmail.com>
2025-08-12 23:04:22 +0800

    .

--- 16/train.py
+++ 16/train.py
@@ -243,7 +243,6 @@ class Train():
                 if i==0:
                     print("test_batch shape:", test_batch.shape, "test_probs shape:", test_probs.shape, 
                           "test_values shape:", test_values.shape, "test_advs shape:", test_advs.shape)
-                    print("test_advs:", test_advs[0].cpu().numpy())
                 if test_data==None: test_data=[test_batch, test_probs, test_values]
                 test_batch = test_batch.to(self.policy_value_net.device)
                 with torch.no_grad(): 
