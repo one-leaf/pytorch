02040b736b4eba146a890ff0ef4c6a9e20a386b5
oneleaf <oneleaf@gmail.com>
2025-08-19 15:58:49 +0800

    .

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -609,7 +609,7 @@ class Train():
                 states.append(step["state"])
                 mcts_probs.append(step["move_probs"])
             
-            if rem>=10:
+            if rem>=len_steps//4 or c==0:
                 print(qval_list[:rem])
                 print(adv_list[:rem])
                 qval_mean = np.mean(qval_list[:rem])
