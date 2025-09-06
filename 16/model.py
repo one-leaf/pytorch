0af98457b3ab8ddbf4ad55767979b6782d326621
oneleaf <oneleaf@gmail.com>
2025-09-06 09:46:04 +0800

    .

--- 16/model.py
+++ 16/model.py
@@ -225,8 +225,8 @@ class PolicyValueNet():
         
         # PPO损失计算        
         ratios = torch.exp(log_probs - torch.log(model_probs + 1e-10))
-        print(ratios)
-        print(adv_batch)
+        print(ratios.shape)
+        print(adv_batch.shape)
         surr1 = ratios * adv_batch
         surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * adv_batch.unsqueeze(1)
         actor_loss = (-torch.min(surr1, surr2)).mean()
