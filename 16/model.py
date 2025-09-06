302f4dc87cc510f56feb641d60f4f497c352e981
oneleaf <oneleaf@gmail.com>
2025-09-06 11:16:02 +0800

    .

--- 16/model.py
+++ 16/model.py
@@ -233,12 +233,12 @@ class PolicyValueNet():
         s_model_probs = torch.log(model_probs.gather(1, actions) + 1e-10) 
         ratios = torch.exp( s_log_probs - s_model_probs)
 
-        print(s_log_probs.shape, s_model_probs.shape, ratios.shape, adv_batch.shape)
+        # print(s_log_probs.shape, s_model_probs.shape, ratios.shape, adv_batch.shape)
         surr1 = ratios * adv_batch
         surr2 = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * adv_batch
-        print(surr1.shape, surr2.shape)
+        # print(surr1.shape, surr2.shape)
         actor_loss = (-torch.min(surr1, surr2)).mean()
-        print(actor_loss.shape)
+        # print(actor_loss.shape)
 
         # MCTS损失计算
         policy_loss = (-torch.sum(mcts_probs * log_probs, 1)).mean() 
