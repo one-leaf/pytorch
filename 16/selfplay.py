098ac373cdbb4e65b4b60086093d01918a5421e9
oneleaf <oneleaf@gmail.com>
2026-01-19 09:44:18 +0800

    。

--- 16/selfplay.py
+++ 16/selfplay.py
@@ -214,7 +214,6 @@ class Train():
 
         # # 新局按Q值走，探索
         # agent = Agent(isRandomNextPiece=False, nextPiecesList=his_pieces )
-        agent.is_replay = False
         agent.limitstep = False
 
         # if his_steps > sample_count:
@@ -351,20 +350,24 @@ class Train():
     def collect_selfplay_data(self):
         """收集自我对抗数据用于训练"""       
         print("TRAIN Self Play starting ...")
+
+        bestmodelfile = model_file+"_best"
+
+        if os.path.exists(model_file):
+            if time.time()-os.path.getmtime(model_file)>60*60*5:
+                print("超过5小时模型都没有更新了，停止训练")
+                time.sleep(60)
+                return
         
         if self.load_model:
             policy_value_net = self.policy_value_net
         else:        
             # 这里用上一次训练的模型作为预测有助于模型稳定
             load_model_file=model_file
-            if os.path.exists(model_file+".bak"):
+            if os.path.exists(bestmodelfile):
+                load_model_file = bestmodelfile
+            elif os.path.exists(model_file+".bak"):
                 load_model_file = model_file+".bak"
-
-            if os.path.exists(load_model_file):
-                if time.time()-os.path.getmtime(load_model_file)>60*60*5:
-                    print("超过5小时模型都没有更新了，停止训练")
-                    time.sleep(60)
-                    return
 
             # 开始游戏
             policy_value_net = PolicyValueNet(GAME_WIDTH, GAME_HEIGHT, GAME_ACTIONS_NUM, model_file=load_model_file)
@@ -372,7 +375,6 @@ class Train():
             self.policy_value_net = policy_value_net
             self.load_model = True
             
-        bestmodelfile = model_file+"_best"
           
         print('start test time:', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
