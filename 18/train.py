21bd8183b81ce423113ebdac14451fa1a41a52e9
oneleaf <oneleaf@gmail.com>
2026-08-07 15:40:00 +0800

    .

--- 18/train.py
+++ 18/train.py
@@ -128,12 +128,13 @@ class PPODataset(torch.utils.data.Dataset):
                     steps_out.append((*step[:8], v_nexts[i]))
 
                 self.data[fn] = steps_out
-                print("R:")
-                print(steps_out[5])
-                print("V_next:")
-                print(steps_out[8])
-                print("prob:")
-                print(np.max(steps_out[1], axis=0))
+                if i==0:
+                    print("R:")
+                    print(steps_out[5])
+                    print("V_next:")
+                    print(steps_out[8])
+                    print("prob:")
+                    print(np.max(steps_out[1]))
                 
             except Exception as e:
                 print(f"file {fn} error: {e}")
